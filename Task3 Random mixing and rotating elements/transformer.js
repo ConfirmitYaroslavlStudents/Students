1329e6f5e80d5100132309cf234174d98aff634b
Alena <Beden-ok@mail.ru>
2013-04-24 02:46:05 +0400

    Refactoring transformer
    cloneNodes are removed.

--- Task3 Random mixing and rotating elements/transformer.js
+++ Task3 Random mixing and rotating elements/transformer.js
@@ -1,64 +1,55 @@
-
 //6. read articles
 
-
-// 1. no div #list, use existing parents
-// 2. declare private var - selected nodes list
-// 3. 
-YUI().use('node', function (Y) {
+// 
+// 1. transformer undefind in page;
+// 2. Recalculation nodelist;
+// 3. do not use cloneNode;
+// 4. read about xml;
 
     function transformer(cssSelector) {
         var result = {};
 
-        result.rotateLeft = function () {
-            var nodelist = Y.all(cssSelector),
-                firstNode = nodelist.item(0);
+        YUI().use('node', function (Y) {
 
-            for (i = 0; i < nodelist.size()-1 ; i++) {
-                var previousNode = Y.all(cssSelector).item(i);
-                previousNode.replace(Y.all(cssSelector).item(i + 1).cloneNode(true));
-            }
-
-            Y.all(cssSelector).item(nodelist.size() - 1).replace(firstNode);
-
-            return this;
-        };
-
-        result.rotateRight = function () {
-            var nodelist = Y.all(cssSelector),
-                lastNode = nodelist.item(nodelist.size() - 1);
-
-            for (i = nodelist.size()-1; i > 0; i--) {
-                var previousNode = Y.all(cssSelector).item(i);
-
-                previousNode.replace(Y.all(cssSelector).item(i-1).cloneNode(true));
-            }
-
-            Y.all(cssSelector).item(0).replace(lastNode);
-
-            return result;
-        };
-
-
-        result.randomize = function () {
             var nodelist = Y.all(cssSelector);
 
-            for (i = 0; i < nodelist.size() ; i++) {
-                var random = Math.floor((Math.random() * (nodelist.size() - 1))),
-                    node = Y.all(cssSelector).item(i),
-                    randomNode = Y.all(cssSelector).item(random);
+            function replaceNodes(node1, node2) {
+                if (node1 == node2)
+                    return;
 
-                if (i == random)
-                    continue;
-                
-                node.replace(Y.all(cssSelector).item(random).cloneNode(true));
-                randomNode.replace(node);
+                var temp = Y.Node.create('temp');
+
+                node1.replace(temp);
+                node2.replace(node1);
+                temp.replace(node2);
             }
 
-            return result;
-        };
+            result.rotateLeft = function () {
+                for (var i = 0; i < nodelist.size() - 1 ; i++)
+                    replaceNodes(Y.all(cssSelector).item(i), Y.all(cssSelector).item(i + 1));
+                
+                return result;
+            };
+
+            result.rotateRight = function () {
+                for (var i = nodelist.size() - 1; i > 0; i--)
+                    replaceNodes(Y.all(cssSelector).item(i), Y.all(cssSelector).item(i-1));
+
+                return result;
+            };
+
+
+            result.randomize = function () {
+                for (var i = 0; i < nodelist.size() - 1; i++) {
+                    var random = Math.floor((Math.random() * (nodelist.size() - 1)));
+
+                    replaceNodes(Y.all(cssSelector).item(i), Y.all(cssSelector).item(random));
+                }
+
+                return result;
+            };
+        });
 
         return result;
+        
     };
-    window.transformer=transformer;
-});

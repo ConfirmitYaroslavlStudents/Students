c6193b2b966fad696e1b3ee49a2a13de1c563961
darknyaz <darknyaz@yandex.ru>
2013-07-19 21:33:40 +0400

    HeapSort is extension. Using Stopwatch.

--- Knyazev/Heap/Heap/SortingTimeTest.cs
+++ Knyazev/Heap/Heap/SortingTimeTest.cs
@@ -8,8 +8,8 @@ namespace Heap
 
 		public static int[] GenerateIntArray(int size)
 		{
-			Random randomNumberGenerator = new Random();
-			int[] result = new int[size];
+			var randomNumberGenerator = new Random();
+			var result = new int[size];
 			for (int i = 0; i < size; ++i)
 				result[i] = randomNumberGenerator.Next(MINNUM, MAXNUM);
 

405e0b276a1e710035ffa3d499e47e230a30a79f
darknyaz <darknyaz@yandex.ru>
2013-08-09 01:37:02 +0400

    Template method in MineSweeper.

---
+++ Knyazev/MineSweeperAfterRefactoring/MineSweeper/Cell.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MineSweeper
+{
+	struct Cell
+	{
+		public int Row, Column;
+
+		public Cell(int row, int column)
+		{
+			Row = row;
+			Column = column;
+		}
+	}
+}

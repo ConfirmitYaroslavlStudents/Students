b2ebb4bac8a7ac871c689ca95eba01b8de261956
ShanisCoder <Shanis394042@gmail.com>
2013-08-11 11:40:30 +0400

    Some refactoring in Tree

--- Ushakov/Tree/Tree/Tree.cs
+++ Ushakov/Tree/Tree/Tree.cs
@@ -6,53 +6,11 @@ using System.Threading.Tasks;
 
 namespace Tree
 {
-    class Node<T> where T : IComparable
-    {
-        public T Value { get; set; }
-        public Node<T> Left { get; set; }
-        public Node<T> Right { get; set; }
-        public int Height { get; private set; }
-
-        public Node(T value)
-        {
-            this.Value = value;
-            Left = null;
-            Right = null;
-            Height = 1;
-        }
-
-        public void SetHeight()
-        {
-            if (Left == null && Right == null)
-            {
-                Height = 1;
-                return;
-            }
-            if (Left == null)
-            {
-                Height = Right.Height + 1;
-                return;
-            }
-            if (Right == null)
-            {
-                Height = Left.Height + 1;
-                return;
-            }
-            Height = Math.Max(Left.Height, Right.Height) + 1;
-        }
-
-        public int GetBalanceFactor()
-        {
-            return (Right != null ? Right.Height : 0) -
-                (Left != null ? Left.Height : 0);
-        }
-    }
-
     class Tree<T> : IEnumerable<T> where T : IComparable
     {
         private Node<T> _root;
 
-        public ITraversing<T> Traversing { get; set; }
+        public Traversing<T> Traversing { get; set; }
 
         public Tree()
         {

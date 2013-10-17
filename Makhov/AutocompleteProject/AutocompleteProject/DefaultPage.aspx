b3228cd50b7df52be7390e409479360ab507072d
Ol5xHd <a.o.makhov@gmail.com>
2013-10-18 03:15:34 +0400

    VS2012, no hard code

---
+++ Makhov/AutocompleteProject/AutocompleteProject/DefaultPage.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="DefaultPage.aspx.cs" Inherits="AutocompleteProject.MainForm" %>
+
+<%@ Register Assembly="MyControl" Namespace="MyControl" TagPrefix="mine" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <%-- Can't eliminate hardcode inside <div> tag.
+             class="yui3-aclist-input" inside MyControlClass tag?--%>
+        <div class="yui3-skin-sam">
+            <label for="AutoC">
+                Numbers from 0 to 20:
+            </label>
+            <br />
+            <mine:MyControlClass ID="AutoC" runat="server" />
+        </div>
+    </form>
+</body>
+</html>
--- Makhov/AutocompleteProject/AutocompleteProject/Properties/AssemblyInfo.cs
+++ Makhov/AutocompleteProject/AutocompleteProject/Properties/AssemblyInfo.cs
@@ -1,20 +1,18 @@
-﻿using System.Web.UI;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MyControl")]
+[assembly: AssemblyTitle("AutocompleteProject")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MyControl")]
+[assembly: AssemblyProduct("AutocompleteProject")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: TagPrefix("MyControl", "mine")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -22,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("76f86725-c268-49f5-9b0d-ffb76cbbbd0e")]
+[assembly: Guid("f8d200f5-4730-4511-953c-796d5276104a")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,8 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]

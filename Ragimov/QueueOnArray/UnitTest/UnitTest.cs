7b43e469ddaf5439615c479f73d1cdef83f3b118
Andrey <andrey@ragimov.info>
2014-07-09 15:50:08 +0400

    Queue on Array with 3 tests

---
+++ Ragimov/QueueOnArray/UnitTest/UnitTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class QueueUnitTest
+    {
+        [TestMethod]
+        public void EnAndDeQueueTest()
+        {
+            var queue = new QueueOnArray.Queue<int>();
+            queue.Enqueue(42);
+            Assert.AreEqual(queue.Dequeue(), 42);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void DeleteFromEmpty_ShouldThrowInvalidOperation()
+        {
+            var queue = new QueueOnArray.Queue<int>();
+            queue.Dequeue();
+        }
+
+        [TestMethod]
+        public void CountIsProperlyChangingTest()
+        {
+            var queue = new QueueOnArray.Queue<int>();
+            queue.Enqueue(42);
+            var temp = queue.Count;
+            queue.Enqueue(37);
+            queue.Enqueue(queue.Dequeue());
+            Assert.AreEqual(queue.Count, temp + 1);
+        }
+    }
+}

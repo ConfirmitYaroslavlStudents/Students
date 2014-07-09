1866acc7a8fae68472e0ad1ea6cf59c7e6956db6
GrimRanger <alovchenkov@gmail.com>
2014-07-09 16:45:04 +0400

    Stack with tests

---
+++ Ovchenkov/StackLibrary/StackLibrary/Elem.cs
@@ -0,0 +1,21 @@
+﻿
+namespace StackLibrary
+{
+    internal class Elem<T>
+    {
+        private T _data;
+        private Elem<T> _next;
+
+        public object Value
+        {
+            get { return _data; }
+            set { _data = (T)value; }
+        }
+
+        public Elem<T> Next
+        {
+            get { return _next; }
+            set { _next = value; }
+        }
+    }
+}

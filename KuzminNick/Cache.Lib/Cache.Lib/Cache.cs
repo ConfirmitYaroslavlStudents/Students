7eb46ed48e522d32d07bd3e3455ff2d298f822e3
Nick <nick.kuzmin.developer@gmail.com>
2014-07-17 00:07:47 +0300

    CacheLibrary [without testing correctness of timer functionality]

---
+++ KuzminNick/Cache.Lib/Cache.Lib/Cache.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+
+namespace CacheLib
+{
+    public class Element<T>
+    {
+        private T _value;
+        private readonly int _hashCode;
+        private int _frequenceUsage;
+        private int _timeLastUsingInSeconds;
+
+        public Element()
+        {
+            FrequencyUsage = 0;
+        }
+
+        public Element(T value)
+            : this()
+        {
+            Value = value;
+            FrequencyUsage = 0;
+        }
+
+        public Element(int hashCode, T value)
+            : this()
+        {
+            _hashCode = hashCode;
+            Value = value;
+        }
+
+        public T Value
+        {
+            get { return _value; }
+            set { _value = value; }
+        }
+        public int HashCode
+        {
+            get { return _hashCode; }
+        }
+
+        public int FrequencyUsage
+        {
+            get { return _frequenceUsage; }
+            set { _frequenceUsage = value; }
+        }
+        public int TimeLastUsingInSeconds
+        {
+            get { return _timeLastUsingInSeconds; }
+            set { _timeLastUsingInSeconds = value; }
+        }
+    }
+
+    public class Cache<T>
+    {
+        private readonly Dictionary<int, Element<T>> _cache;
+        private int _capacity;
+        private int _limitTimeStoringInCacheInSeconds;
+        private Stopwatch _cacheStopwatch = new Stopwatch();
+        private TimerCallback _timerCallingRefreshing;
+
+        public Cache()
+        {
+            _cache = new Dictionary<int, Element<T>>();
+            _capacity = 10;
+            _limitTimeStoringInCacheInSeconds = 10;
+            ActivateStopwatch();
+            ActivateTimerRefresher();
+        }
+
+        public Cache(int capacity)
+        {
+            _cache = new Dictionary<int, Element<T>>();
+            _capacity = capacity;
+            _limitTimeStoringInCacheInSeconds = 10;
+            ActivateStopwatch();
+            ActivateTimerRefresher();
+        }
+
+        public Cache(int capacity, int limitTimeStoringInCacheInSeconds)
+        {
+            _capacity = capacity;
+            _limitTimeStoringInCacheInSeconds = limitTimeStoringInCacheInSeconds;
+            ActivateStopwatch();
+            ActivateTimerRefresher();
+        }
+
+        public int LimitTimeStoringInCacheInSeconds
+        {
+            get { return _limitTimeStoringInCacheInSeconds; }
+            set { _limitTimeStoringInCacheInSeconds = value; }
+        }
+
+        public int Capacity
+        {
+            get { return _capacity; }
+            set
+            {
+                if(value > 0 && value > _capacity)
+                    _capacity = value;
+
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public int CurrentSize
+        {
+            get { return _cache.Count; }
+        }
+
+        private int KeyMostUsedElement { get; set; }
+
+        public Element<T> GetItem(IEnumerable<Element<T>> collection, int hashCode)
+        {
+            if (_cache.ContainsKey(hashCode))
+            {
+                _cache[hashCode].FrequencyUsage++;
+                _cache[hashCode].TimeLastUsingInSeconds = _cacheStopwatch.Elapsed.Seconds;
+                return _cache[hashCode];
+            }
+
+            // 'slow' getting data from some IEnumerable Structure
+            foreach (var element in collection)
+            {
+                if (!element.GetHashCode().Equals(hashCode)) continue;
+
+                AddItem(element);
+                return element;
+            }
+
+            throw new NullReferenceException();
+        }
+        private void ActivateStopwatch()
+        {
+            _cacheStopwatch.Start();
+        }
+
+        private void ActivateTimerRefresher()
+        {
+            _timerCallingRefreshing = new TimerCallback(RefreshCache);
+            var time = new Timer(_timerCallingRefreshing, null, 0, 10);
+        }
+
+        public void AddItem(Element<T> element)
+        {
+            if (CacheIsFull())
+            {
+                var keyRemovedElement = FindKeyLeastUsedElement();
+                _cache.Remove(keyRemovedElement);
+            }
+
+            _cache.Add(element.Value.GetHashCode(), element);
+        }
+
+        private bool CacheIsFull()
+        {
+            return Capacity == CurrentSize;
+        }
+
+        private int FindKeyLeastUsedElement()
+        {
+            var keyRemovedElement = KeyMostUsedElement;
+            foreach (var item in _cache)
+            {
+                if (item.Value.FrequencyUsage == 0)
+                {
+                    keyRemovedElement = item.Key;
+                    break;
+                }
+
+                if (item.Value.FrequencyUsage < keyRemovedElement)
+                {
+                    keyRemovedElement = item.Key;
+                }
+            }
+            return keyRemovedElement;
+        }
+
+        public string GetAllElementsCacheInStringFormat()
+        {
+            var allElementsInStringFormat = "";
+            foreach (var cacheElement in _cache)
+            {
+                Element<T> element = cacheElement.Value;
+                allElementsInStringFormat += "|" + element.Value;
+            }
+
+            return allElementsInStringFormat;
+        }
+
+        private void RefreshCache(object state)
+        {
+            var keysRemovingElements = _cache
+                .Where(
+                    pair =>
+                        pair.Value.TimeLastUsingInSeconds - _cacheStopwatch.Elapsed.Seconds >=
+                        LimitTimeStoringInCacheInSeconds)
+                .Select(pair => pair.Key)
+                .ToList();
+
+            foreach (var keyRemovingElement in keysRemovingElements)
+            {
+                _cache.Remove(keyRemovingElement);
+            }
+        }
+    }
+}
---
+++ KuzminNick/Cache.Lib/CacheLib.UnitTests/UnitTest.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CacheLib.UnitTests
+{
+    [TestClass]
+    public class CacheUnitTests
+    {
+        [TestMethod]
+        public void CacheUnitTest_AddingElementsInCache()
+        {
+            var cache = new Cache<string>();
+            var list = new List<Element<string>>();
+            for (var i = 0; i < 10; i++)
+            {
+                var randomString = Path.GetRandomFileName();
+                var keyElement = randomString.GetHashCode();
+                var randomElement = new Element<string>(keyElement, randomString);
+                cache.AddItem(randomElement);
+            }
+
+            cache.AddItem(new Element<string>("Test".GetHashCode(), "Test"));
+            Assert.AreEqual("Test", cache.GetItem(list, "Test".GetHashCode()).Value);
+        }
+
+        [TestMethod]
+        public void CacheUnitTest_AddingElements_FromSlowCollection_InCache()
+        {
+            var cache = new Cache<string>();
+            var list = new List<Element<string>>();
+            for (var i = 0; i < 9; i++)
+            {
+                var randomString = Path.GetRandomFileName();
+                var keyElement = randomString.GetHashCode();
+                var randomElement = new Element<string>(keyElement, randomString);
+                list.Add(randomElement);
+            }
+
+            cache.AddItem(new Element<string>("Test".GetHashCode(), "Test"));
+            Assert.AreEqual("Test", cache.GetItem(list, "Test".GetHashCode()).Value);
+        }
+
+        [TestMethod]
+        public void CacheUnitTest_AddingElements_ExceedingMaxLimitOfCache()
+        {
+            var cache = new Cache<string>(10);
+            var list = new List<Element<string>>();
+            for (var i = 0; i < 16; i++)
+            {
+                var randomString = Path.GetRandomFileName();
+                var keyElement = randomString.GetHashCode();
+                var randomElement = new Element<string>(keyElement, randomString);
+                list.Add(randomElement);
+            }
+
+            foreach (var element in list)
+            {
+                cache.AddItem(element);
+            }
+
+            Assert.AreEqual(10, cache.CurrentSize);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void CacheUnitTest_GettingElementNotIncludedInCache()
+        {
+            var cache = new Cache<string>(10);
+            var list = new List<Element<string>>();
+            for (var i = 0; i < 5; i++)
+            {
+                var randomString = Path.GetRandomFileName();
+                var keyElement = randomString.GetHashCode();
+                var randomElement = new Element<string>(keyElement, randomString);
+                list.Add(randomElement);
+            }
+
+            foreach (var element in list)
+            {
+                cache.AddItem(element);
+            }
+
+            cache.GetItem(list, "Not Included Item".GetHashCode());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void CacheUnitTest_UncorrectChangingOfCapacityProperty()
+        {
+            var cache = new Cache<string>(10);
+            cache.Capacity = 5;
+        }
+
+        [TestMethod]
+        public void CacheUnitTest_AutoRemovingLeastUsedItem_FromCache()
+        {
+            var cache = new Cache<string>();
+            var list = new List<Element<string>>();
+            for (var i = 0; i < 10; i++)
+            {
+                var keyElement = i.ToString().GetHashCode();
+                var item = new Element<string>(keyElement, i.ToString());
+                list.Add(item);
+            }
+
+            foreach (var element in list)
+            {
+                cache.AddItem(element);
+            }
+
+            for (var i = 0; i < 9; i++)
+            {
+                Console.WriteLine(i);
+                cache.GetItem(list, i.ToString().GetHashCode()); // additionly, it increases 'frequenceUsage'-value of Element<T>
+            }
+
+            cache.AddItem(new Element<string>("9 will be replaced as least used item in cache".GetHashCode(), "9 will be replaced as least used item in cache"));
+            Assert.AreEqual("|0|1|2|3|4|5|6|7|8|9 will be replaced as least used item in cache", cache.GetAllElementsCacheInStringFormat());
+        }
+    }
+}

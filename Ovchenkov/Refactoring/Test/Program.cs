afb7c257df2691b6390194513e8f5cabf37cee13
GrimRanger <alovchenkov@gmail.com>
2014-08-12 17:39:01 +0400

    Added file

---
+++ Ovchenkov/Refactoring/Test/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using Refactoring;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer("nobody");
+            var statement = new Statement(customer);
+            Console.WriteLine(statement.GetStatement(new StringStatement()));
+        }
+    }
+}

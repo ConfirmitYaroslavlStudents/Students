9a6a6794679ef41615b873ca8b278b145f922865
prototype9449 <prototype9449@yandex.ru>
2014-08-15 00:39:57 +0400

    unfinished library

---
+++ Popov/ColorLibrary/ColorLibrary/interfaces.cs
@@ -0,0 +1,11 @@
+﻿
+namespace ColorLibrary
+{
+    public interface IColor
+    {
+        string DoWith(IColor color);
+        Red ToRed();
+        Green ToGreen();
+    }
+   
+}
---
+++ Popov/ColorLibrary/ColorWork/TestColors.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ColorWork
+{
+    [TestClass]
+    public class TestColors
+    {
+        [TestMethod]
+        public void TestColor()
+        {
+
+        }
+    }
+}

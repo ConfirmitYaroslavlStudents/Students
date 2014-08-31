f4cf6215c1d0d1bdf1ebed57d8a7acc10799a128
prototype9449 <prototype9449@yandex.ru>
2014-08-31 15:07:34 +0400

    BlankFiller class added

---
+++ Hospital_Alferov_Vinogradov_Popov_team/Hospital/TestBlankFiller/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using Blank;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shared;
+
+namespace TestBlankFiller
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //var blank = new BlankFiller(@"D:\test.txt");
+            
+            //File.WriteAllText(@"D:\tre.txt", blank.FillBlank(person, analyse, template));
+            
+        }
+    }
+}

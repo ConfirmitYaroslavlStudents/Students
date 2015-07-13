b19243f8d7b397393ccf0ec2077d60d3230f2aab
Vladimir Hisoka <vaxiktru@gmail.com>
2015-07-13 18:14:14 +0300

    folder Hohrenko created

---
+++ 2015/Hohrenko/myList/myList/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> testList = new MyList<string>();
+
+            testList.Add("one");
+            testList.Add("two");
+            testList.Add("three");
+            testList.Add("four");
+            testList.Add("five");
+            testList.Add("six");
+            testList.Add("seven");
+            testList.Add("eight");
+
+            
+
+           // testList.PrintContents();
+
+            //testList.Remove("six");
+            //testList.PrintContents();
+            testList.Insert(7, "insert1");
+            testList.Insert(9, "insert2");
+
+            
+            foreach (var s in testList)
+            {
+                Console.WriteLine(s);
+            }
+            
+        }
+    }
+}

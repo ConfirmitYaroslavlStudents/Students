9a3b91796a5d5875923786642a470cfae10561cd
Mikhail Bolshakov <veujin@gmail.com>
2015-07-17 02:00:51 +0300

    Implemented main method

---
+++ 2015/Bolshakov/MP3_tager/MP3_tager/CodeBehind.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MP3_tager
+{
+    static class CodeBehind
+    {
+        public static void TagFile(string path, string pattern)
+        {
+
+        }
+
+        private static void InsertTo(string path, Dictionary<TagTypes,String> tags)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

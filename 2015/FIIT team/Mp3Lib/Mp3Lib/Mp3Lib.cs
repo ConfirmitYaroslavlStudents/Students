9b55e30b4e858d057d43cefbccb9b65b319d187b
Vladimir Hisoka <vaxiktru@gmail.com>
2015-07-18 14:53:02 +0300

    Rename function added. And some fixes

--- 2015/FIIT team/Mp3Lib/Mp3Lib/Mp3Lib.cs
+++ 2015/FIIT team/Mp3Lib/Mp3Lib/Mp3Lib.cs
@@ -1,15 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 
 namespace Mp3Lib
 {
     public class Mp3Lib
     {
-        private Dictionary<string, List<int>> _commandList = new Dictionary<string, List<int>>()
+        private Dictionary<string, int[]> _commandList = new Dictionary<string, int[]>()
         {
-            {"help", new List<int>(){1, 2}},
-            {"raname", new List<int>(){3}},
+            {"help",  new [] {1, 2}},
+            {"rename", new [] {3}},
         };
 
         private string[] _args;
@@ -36,17 +38,42 @@ namespace Mp3Lib
 
         public void ExecuteCommand()
         {
-            string command = _args[0];
-            IEnumerable<string> otherArgs = new ArraySegment<string>(_args, 1, _args.Length-1);
 
+            string command = _args[0];
+            CheckArgs(command);
+          
             switch (command)
             {
-                case "help" : ShowHelp(); break;
+                case "help" : ShowHelp();
+                    break;
 
-                case "rename" :
+                case "rename" : Rename(_args);
                     break;
             }
 
+        }
+
+        private void Rename(string[] args)
+        {
+            var foldPath = args[1];
+            var pattern = args[2];
+            var dir = new DirectoryInfo(foldPath);
+            Console.WriteLine(dir.FullName);
+            foreach (var file in dir.GetFiles("*.mp3"))
+            {
+                var mp3 = TagLib.File.Create(file.FullName);
+
+                //file.MoveTo(file.DirectoryName + @"\" + GetNewNameByPattern(pattern, mp3) + ".mp3");
+                Console.WriteLine(file.DirectoryName + @"\" + GetNewNameByPattern(pattern, mp3) + ".mp3");
+            }
+        }
+
+        private string GetNewNameByPattern(string pattern, TagLib.File mp3)
+        {
+            var s = new StringBuilder(pattern);
+            s.Replace("{artist}", mp3.Tag.FirstPerformer);
+            s.Replace("{title}", mp3.Tag.Title);
+            return s.ToString();
         }
     }
 }
--- 2015/FIIT team/Mp3Lib/Mp3Lib/Program.cs
+++ 2015/FIIT team/Mp3Lib/Mp3Lib/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Mp3Lib
 {
@@ -8,15 +12,14 @@ namespace Mp3Lib
         {
             Mp3Lib app = new Mp3Lib(args);
             if (args.Length == 0)
-            {      
+            {
+                Console.WriteLine( "maloargov");
                app.ShowHelp();
                Environment.Exit(0);
             }
 
             app.ExecuteCommand();
-
-            
-
+ 
         }
     }
 }

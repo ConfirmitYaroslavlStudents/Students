a6f67636da197f628fe207fe6cf7a60e1dcbc744
mokeev1995 <andrey@mokeev1995.ru>
2015-07-19 05:16:38 +0300

    fixed indentation

--- 2015/Mokeev/mp3lib/mp3_lib/Mp3File.cs
+++ 2015/Mokeev/mp3lib/mp3_lib/Mp3File.cs
@@ -7,10 +7,10 @@ using System.Threading.Tasks;
 
 namespace mp3_lib
 {
-    public class Mp3File
-    {
-	    private byte[] _id3v1Data;
-	    private byte[] _id3v1ExtendedData;
+	public class Mp3File
+	{
+		private byte[] _id3v1Data;
+		private byte[] _id3v1ExtendedData;
 
 		public string Title { get; set; }
 		public string Artist { get; set; }
@@ -21,29 +21,29 @@ namespace mp3_lib
 		public ushort TrackId { get; set; }
 		public ushort Genre { get; set; }
 
-	    public Mp3File(string file)
-	    {
-			if(!File.Exists(file)) throw new FileNotFoundException("File not found", file);
+		public Mp3File(string file)
+		{
+			if (!File.Exists(file)) throw new FileNotFoundException("File not found", file);
 
 			var fileData = new MemoryStream();
 
-		    using (var fs = new FileStream(file, FileMode.Open))
-		    {
+			using (var fs = new FileStream(file, FileMode.Open))
+			{
 				fs.CopyTo(fileData);
-		    }
+			}
 
-		    var fileDataArray = fileData.ToArray();
+			var fileDataArray = fileData.ToArray();
 
 
-		    var data = fileDataArray.Reverse().Take(128).Reverse().ToArray();
+			var data = fileDataArray.Reverse().Take(128).Reverse().ToArray();
 
-		    var tagCode = data.Take(3).ToArray();
-		    
+			var tagCode = data.Take(3).ToArray();
+
 			var t1 = Encoding.UTF8.GetString(tagCode);
 			var t2 = Encoding.ASCII.GetString(tagCode);
 
-		    if (Encoding.UTF8.GetString(tagCode) == "TAG" || Encoding.ASCII.GetString(tagCode) == "TAG")
-			    _id3v1Data = data;
-	    }
-    }
+			if (Encoding.UTF8.GetString(tagCode) == "TAG" || Encoding.ASCII.GetString(tagCode) == "TAG")
+				_id3v1Data = data;
+		}
+	}
 }

78fa39d89430412fac4bd9ee1f1c9a9f4ae95b7b
mokeev1995 <andrey@mokeev1995.ru>
2015-07-19 05:11:49 +0300

    mp3 lib Solution added
    mp3 solution added and wrote first code for mp3 library

---
+++ 2015/Mokeev/mp3lib/ConsoleMp3TagEditor/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using mp3_lib;
+
+namespace ConsoleMp3TagEditor
+{
+	public static class Program
+	{
+		public static void Main(string[] args)
+		{
+			var mp3 = new Mp3File(Environment.CurrentDirectory + @"/Changer - The biggest in the world.mp3");
+		}
+	}
+}
---
+++ 2015/Mokeev/mp3lib/mp3_lib/Mp3File.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mp3_lib
+{
+    public class Mp3File
+    {
+	    private byte[] _id3v1Data;
+	    private byte[] _id3v1ExtendedData;
+
+		public string Title { get; set; }
+		public string Artist { get; set; }
+		public string Album { get; set; }
+		public ushort Year { get; set; }
+		public string Comment { get; set; }
+		private byte ZeroByte { get; set; }
+		public ushort TrackId { get; set; }
+		public ushort Genre { get; set; }
+
+	    public Mp3File(string file)
+	    {
+			if(!File.Exists(file)) throw new FileNotFoundException("File not found", file);
+
+			var fileData = new MemoryStream();
+
+		    using (var fs = new FileStream(file, FileMode.Open))
+		    {
+				fs.CopyTo(fileData);
+		    }
+
+		    var fileDataArray = fileData.ToArray();
+
+
+		    var data = fileDataArray.Reverse().Take(128).Reverse().ToArray();
+
+		    var tagCode = data.Take(3).ToArray();
+		    
+			var t1 = Encoding.UTF8.GetString(tagCode);
+			var t2 = Encoding.ASCII.GetString(tagCode);
+
+		    if (Encoding.UTF8.GetString(tagCode) == "TAG" || Encoding.ASCII.GetString(tagCode) == "TAG")
+			    _id3v1Data = data;
+	    }
+    }
+}

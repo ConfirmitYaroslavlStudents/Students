c709537e9e145ced9f4c3b881307bebf91af556e
mokeev1995 <andrey@mokeev1995.ru>
2015-07-22 18:32:24 +0300

    completely changed code

--- 2015/Mokeev/mp3lib/ConsoleMp3TagEditor/Program.cs
+++ 2015/Mokeev/mp3lib/ConsoleMp3TagEditor/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using mp3_lib;
 
 namespace ConsoleMp3TagEditor
@@ -11,9 +8,87 @@ namespace ConsoleMp3TagEditor
 	{
 		public static void Main(string[] args)
 		{
-			var mp3 = new Mp3File(Environment.CurrentDirectory + @"/Changer - The biggest in the world.mp3");
+			if (!ArgsValid(args)) throw new ArgumentException("Wrong arguments passed", "args");
 
-			Console.WriteLine(mp3.Title);
+			var data = ExtactArgs(args);
+
+			var mp3 = new Mp3File(data["-file"]);
+
+			var renamer = new Mp3RenamerFromFileName(mp3);
+
+			renamer.Start(data["-mask"]);
+			
+			Console.ReadKey();
 		}
+
+		public static bool ArgsValid(string[] args)
+		{
+			if (args.Length != 4)
+			{
+				Console.WriteLine("Expected usage: {0} -file \"[path to file]\" -mask \"[mask for changing title]\"");
+				return false;
+			}
+
+			var hasFilePath = false;
+			var hasMask = false;
+			foreach (var str in args)
+			{
+				switch (str)
+				{
+					case "-file":
+						hasFilePath = true;
+						break;
+					case "-mask":
+						hasMask = true;
+						break;
+				}
+			}
+
+			var hasValidArgs = hasMask && hasFilePath;
+			if (!hasValidArgs)
+			{
+				if (!hasMask && !hasFilePath)
+				{
+					Console.WriteLine("You don't append correct file path and mask for mp3.");
+				}
+				else if (!hasFilePath)
+				{
+					Console.WriteLine("You don't append correct file path.");
+				}
+				else
+				{
+					Console.WriteLine("You don't append correct mask for mp3.");
+				}
+
+				return false;
+			}
+
+			if (args[0] == "-file" && args[1] == "-mask" || args[0] == "-mask" && args[1] == "-file")
+			{
+				Console.WriteLine("You don't append correct file path and mask for mp3.");
+				return false;
+			}
+
+			return true;
+		}
+
+		public static Dictionary<string, string> ExtactArgs(IEnumerable<string> args)
+		{
+			var q = new Queue<string>();
+			var retDict = new Dictionary<string, string>();
+			foreach (var arg in args)
+			{
+				if (q.Count == 0)
+				{
+					q.Enqueue(arg);
+					continue;
+				}
+
+				retDict.Add(q.Dequeue(), arg);
+			}
+
+			return retDict;
+		}
+
 	}
 }
--- 2015/Mokeev/mp3lib/mp3_lib/Mp3File.cs
+++ 2015/Mokeev/mp3lib/mp3_lib/Mp3File.cs
@@ -4,78 +4,56 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Id3;
+using Id3.Frames;
+
 // ReSharper disable InconsistentNaming
 
 namespace mp3_lib
 {
 	public class Mp3File
 	{
-		private byte[] _id3v1Data;
-		private byte[] _id3v1ExtendedData;
+		private Mp3Stream mp3File { get; set; }
+		public string FilePath { get; private set; }
+		private Id3Tag _tags { get; set; }
 
-		private string _filePath;
+		public string Title
+		{
+			get
+			{
+				return _tags.Title;
+			}
+			set { _tags.Title.Value = value; Set(); }
+		}
 
-		private string _title;
-		private string _artist;
-		private string _album;
-		private ushort _year;
-		private string _comment;
-		private ushort _trackId;
-		private ushort _genre;
-
-		public string Title { get { return _title; }  }
-		public string Artist { get { return _artist; } }
-		public string Album { get { return _album; }  }
-		public ushort Year { get { return _year; } }
-		public string Comment { get { return _comment; }  }
-		private byte ZeroByte { get; set; }
-		public ushort TrackId { get { return _trackId; } }
-		public ushort Genre { get { return _genre; } }
+		public string Artist { get { return _tags.Artists; } set { _tags.Artists.TextValue = value; Set(); } }
+		public string Album { get { return _tags.Album; } set { _tags.Album.Value = value; Set(); } }
+		public int? Year { get { return _tags.Year.Value; } set { _tags.Year.Value = value; Set(); } }
+		public string Comment { get { return _tags.Comments.ToString(); } set { _tags.Comments.Clear(); _tags.Comments.Add(new CommentFrame(){Comment = value}); Set(); } }
+		public ushort TrackId { get { return Convert.ToUInt16(_tags.Track.TrackCount); } set { _tags.Track.Value = value; Set(); } }
+		public ushort Genre { get { return Convert.ToUInt16(_tags.Genre); }  set { _tags.Genre.Value = value.ToString(); Set(); } }
 
 		public Mp3File(string file)
 		{
 			if (!File.Exists(file)) throw new FileNotFoundException("File not found", file);
-			_filePath = file;
+			FilePath = file;
 
-			var fileData = new MemoryStream();
+			var fs = new FileStream(file, FileMode.Open, FileAccess.ReadWrite);
 
-			using (var fs = new FileStream(file, FileMode.Open))
-			{
-				fs.CopyTo(fileData);
-			}
+			mp3File = new Mp3Stream(fs, Mp3Permissions.ReadWrite);
 
-			var fileDataArray = fileData.ToArray();
+			if (!mp3File.HasTags) return;
+			var id3v1exists = mp3File.HasTagOfFamily(Id3TagFamily.Version1x);
+			var id3v2exists = mp3File.HasTagOfFamily(Id3TagFamily.Version2x);
 
+			if (!id3v2exists && id3v1exists) _tags = mp3File.GetTag(Id3TagFamily.Version1x);
 
-			var data = fileDataArray.Reverse().Take(128).Reverse().ToArray();
+			_tags = mp3File.GetTag(Id3TagFamily.Version2x);
+		}
 
-			var tagCode = data.Take(3).ToArray();
-
-			if (Encoding.Default.GetString(tagCode) == "TAG")
-			{
-				_id3v1Data = data;
-				var tmp = fileDataArray.Reverse().Take(355).Reverse().ToArray();
-				var extendedData = tmp.Take(227).ToArray();
-
-				var tmpData = data.Reverse().Take(125).Reverse().ToArray();
-				_title = Encoding.Default.GetString(tmpData.Take(30).ToArray());
-
-				tmpData = tmpData.Reverse().Take(95).Reverse().ToArray();
-				_artist = Encoding.Default.GetString(tmpData.Take(30).ToArray());
-
-				tmpData = tmpData.Reverse().Take(65).Reverse().ToArray();
-				_album = Encoding.Default.GetString(tmpData.Take(30).ToArray());
-
-				tmpData = tmpData.Reverse().Take(35).Reverse().ToArray();
-				ushort.TryParse(Encoding.Default.GetString(tmpData.Take(4).ToArray()), out _year);
-
-
-				if (Encoding.Default.GetString(extendedData.Take(4).ToArray()) == "TAG+")
-				{
-					
-				}
-
-			}
+		private void Set()
+		{
+			mp3File.WriteTag(_tags, WriteConflictAction.Replace);
 		}
 	}
 }

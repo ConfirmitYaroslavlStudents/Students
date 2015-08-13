d11c0c15bba1a5f62deb80b67745985aa978c3d6
Artem <cinquestyle@gmail.com>
2015-08-13 18:22:14 +0300

    Analyse Tests

---
+++ 2015/FIIT team/Mp3Tager/Mp3Tager/Tests/AnalyseCommandTests.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using CommandCreation;
+using FileLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tests.Fakes;
+
+namespace Tests
+{
+    [TestClass]
+    public class AnalyseCommandTests
+    {
+        [TestMethod]
+        public void Analyse_Common_Successful()
+        {
+            var sourceFolder = @"D:\music\";
+
+            var mp3Files = new List<IMp3File>
+            {
+                new FakeMp3File(new Mp3Tags
+                {
+                    Album = "TestAlbum1", Artist = "TestArtist1", Genre = "TestGenre1", Title = "TestTitle1", Track = 1
+                }, sourceFolder + "TestArtist1 - TestTitle2.mp3"),
+
+                new FakeMp3File(new Mp3Tags
+                {
+                    Album = "TestAlbum2", Artist = "TestArtist2", Genre = "TestGenre2", Title = "TestTitle2", Track = 2
+                }, sourceFolder + "2.mp3"),
+
+                new FakeMp3File(new Mp3Tags
+                {
+                    Album = "TestAlbum3", Artist = "TestArtist3", Genre = "TestGenre3", Title = "TestTitle3", Track = 3
+                }, sourceFolder + "3.mp3"),
+            };
+
+            var fakeSystemSource = new FakeSystemSource(sourceFolder, mp3Files);
+
+            var fakeWriter = new FakeWriter();
+            var analyser = new AnalyseCommand(fakeSystemSource, @"{artist} - {title}", fakeWriter);
+
+            analyser.Execute();
+
+            Assert.AreEqual("{title} in file name: TestTitle2; {title} in tags: TestTitle1\n", fakeWriter.Stream.ToString());
+        }
+    }
+}
--- 2015/FIIT team/Mp3Tager/Mp3Tager/Tests/Fakes/FakeSystemSource.cs
+++ 2015/FIIT team/Mp3Tager/Mp3Tager/Tests/Fakes/FakeSystemSource.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using FileLib;
 
 namespace Tests.Fakes
@@ -7,18 +8,23 @@ namespace Tests.Fakes
     class FakeSystemSource : ISource
     {
         public string SourceFolder { get; private set; }
+
+        private readonly List<IMp3File> _mp3Files;
+
+        public FakeSystemSource(string sourceFilder, List<IMp3File> mp3Files)
+        {
+            SourceFolder = sourceFilder;
+            _mp3Files = mp3Files;
+        }
+
         public IEnumerable<string> GetFileNames()
         {
-            List<string> filenames = new List<string>();
-            return filenames;
+            return _mp3Files.Select(mp3File => mp3File.FullName).ToList();
         }
 
         public IEnumerable<IMp3File> GetFiles()
         {
-            List<IMp3File> mp3Files = new List<IMp3File>();
-            //add bunch of FakeMp3File objects
-
-            return mp3Files;
+            return _mp3Files;
         }
     }
 }

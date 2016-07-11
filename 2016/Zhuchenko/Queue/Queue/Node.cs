b6a7d46f276eb3c8a8d7d4bd9a365bb905b7f4e4
Sergey Petrov <Sergey.Petrov@confirmit.com>
2016-07-11 10:03:46 +0300

    Zhuchenko queue

---
+++ 2016/Zhuchenko/Queue/Queue/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    public class Node<T>
+    {
+        public T Value { set; get; }
+        public Node<T> NextNode { set; get; }
+
+        public Node(T value)
+        {
+            Value = value;
+            NextNode = null;
+        }
+    }
+}

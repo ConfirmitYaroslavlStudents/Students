56cecaea859af52b8a14ab3270b420c5232c86ec
donalen <alenademich@gmail.com>
2016-07-11 10:05:32 +0300

    Stack (#30)
    Stack

---
+++ 2016/Demchenko/Stack/TestsFolder/PeekTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Stack_1;
+
+namespace StackTests
+{
+    [TestClass]
+    public class PeekTest
+    {
+        [TestMethod]
+        public void PeekMethod()
+        {
+            IStack<int> stack = new Stack<int>();
+            stack.Push(5);
+            stack.Push(7);
+            stack.Push(10);
+            Assert.AreEqual(10, stack.Peek());
+            Assert.AreEqual(10, stack.Peek());
+            stack.Push(3);
+            stack.Pop();
+            Assert.AreEqual(10, stack.Peek());
+        }
+    }
+}

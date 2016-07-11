f97860d106656e57f87fff1d855c66d163378cb4
donalen <alenademich@gmail.com>
2016-07-11 03:46:05 +0300

    Add files via upload

---
+++ 2016/Demchenko/Stack/StackFolder/Node.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_1
+{
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+    }
+}
---
+++ 2016/Demchenko/Stack/StackFolder/StackArray.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Stack_1
+{
+    public class StackArray<T> : IStack<T>
+    {
+        private const int DefaultCapacity = 8;
+
+        private T[] _items;
+
+        public int Count { get; private set; }
+
+        public StackArray()
+        {
+            Count = 0;
+            _items = new T[DefaultCapacity];
+        }
+
+        public StackArray(IEnumerable<T> collection)
+        {
+            if (collection == null)
+            {
+                throw new ArgumentNullException("collection");
+            }
+
+            Count = 0;
+            _items = new T[DefaultCapacity];
+
+            foreach (var item in collection)
+            {
+                Push(item);
+            }
+        }
+
+        public StackArray(int capacity)
+        {
+            if (capacity < 0)
+            {
+                throw new ArgumentOutOfRangeException("capacity");
+            }
+            Count = 0;
+            _items = capacity > DefaultCapacity ? new T[capacity] : new T[DefaultCapacity];
+        }
+
+        public void Push(T item)
+        {
+            if (Count == _items.Length)
+            {
+                var newArray = new T[2 * _items.Length];
+                Array.Copy(_items, 0, newArray, 0, Count);
+                _items = newArray;
+            }
+            _items[Count++] = item;
+        }
+
+        public T Pop()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            return _items[--Count];
+        }
+
+        public T Peek()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            return _items[Count - 1];
+        }
+
+        public void Clear()
+        {
+            if (Count > 0)
+            {
+                Array.Clear(_items, 0, Count);
+                Count = 0;
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < Count; ++i)
+            {
+                yield return _items[Count - i - 1];
+            }
+        }
+    }
+}

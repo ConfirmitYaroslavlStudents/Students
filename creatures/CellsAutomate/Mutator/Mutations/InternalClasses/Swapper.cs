77cc6db35192e9569853658ee1beff7815f528c3
donalen <alenademich@gmail.com>
2016-07-24 16:32:08 +0300

    Swapper + SwapTests

---
+++ creatures/CellsAutomate/Mutator/Mutations/InternalClasses/Swapper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Creatures.Language.Commands;
+using Creatures.Language.Commands.Interfaces;
+
+namespace CellsAutomate.Mutator.Mutations.InternalClasses
+{
+    internal class Swapper
+    {
+        private ICommand[] _commands;
+        public Swapper(IEnumerable<ICommand> commands)
+        {
+            _commands = commands.ToArray();
+        }
+
+        public ICommand[] SwapCommand(int firstSwapIndex, int secondSwapIndex)
+        {
+            Swap(firstSwapIndex, secondSwapIndex);
+            return _commands;
+        }
+
+        private void Swap(int firstSwapIndex, int secondSwapIndex)
+        {
+            var newCommands = new ICommand[_commands.Length];
+            Array.Copy(_commands, 0, newCommands, 0, firstSwapIndex);
+            newCommands[firstSwapIndex] = _commands[secondSwapIndex];
+            Array.Copy(_commands, firstSwapIndex + 1, newCommands, firstSwapIndex + 1, secondSwapIndex - firstSwapIndex - 1);
+            newCommands[secondSwapIndex] = _commands[firstSwapIndex];
+            Array.Copy(_commands, secondSwapIndex + 1, newCommands, secondSwapIndex + 1, _commands.Length - secondSwapIndex - 1);
+            _commands = newCommands;
+        }
+    }
+}
--- creatures/Creaturestests/MutatorTests/SwapCommandMutationTest.cs
+++ creatures/Creaturestests/MutatorTests/SwapCommandMutationTest.cs
@@ -11,12 +11,12 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Creaturestests.MutatorTests
 {
     [TestClass]
-    public class DublicateCommandMutationTest
-    {      
+    public class SwapCommandMutationTest
+    {
         private CommandsEqualityComparer _comparer = new CommandsEqualityComparer();
 
         [TestMethod]
-        public void DublicateCommandOnTheSpot()
+        public void SwapCommands()
         {
             var precommands =
                new StringBuilder()
@@ -27,27 +27,26 @@ namespace Creaturestests.MutatorTests
                    .AppendLine("endif");
             var commands = new Parser().ProcessCommands(precommands.ToString()).ToArray();
 
-            var indexOfCommandToDublicate = 1;
-            var indexWhereSheWill = 5;
+            var firstSwapIndex = 1;
+            var secondSwapIndex = 3;
 
-            var newCommands = new DublicateCommandMutatiton(
-                new SpecialRandom(indexOfCommandToDublicate,indexWhereSheWill)
+            var newCommands = new SwapCommandMutation(
+                new SpecialRandom(firstSwapIndex, secondSwapIndex)
                 ).Transform(commands);
 
-            var newPreCommands=
+            var newPreCommands =
                 new StringBuilder()
                    .AppendLine("int zero")
-                   .AppendLine("zero = 0")
-                   .AppendLine("if zero then")
                    .AppendLine("stop")
-                   .AppendLine("endif")
-                   .AppendLine("zero = 0");
+                   .AppendLine("if zero then")
+                   .AppendLine("zero = 0")
+                   .AppendLine("endif");
             var parsedNewCommands = new Parser().ProcessCommands(newPreCommands.ToString()).ToArray();
 
             Assert.AreEqual(parsedNewCommands.Length, newCommands.Length);
             for (int i = 0; i < newCommands.Length; i++)
             {
-                Assert.IsTrue(_comparer.IsEqual(newCommands[i],parsedNewCommands[i]));
+                Assert.IsTrue(_comparer.IsEqual(newCommands[i], parsedNewCommands[i]));
             }
         }
     }

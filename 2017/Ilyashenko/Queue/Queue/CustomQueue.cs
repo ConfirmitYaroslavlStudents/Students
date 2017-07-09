98178ea4d14c4fb88a788b4450c28a2a6f50dae6
Fenixton <Fenixton@users.noreply.github.com>
2017-07-10 00:17:40 +0300

    Ilyashenko. Data structure. Queue

---
+++ 2017/Ilyashenko/Queue/Queue/CustomQueue.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Queue
+{
+    public class CustomQueue<T>
+    {
+        private int _defaultCapacity = 10;
+        private int _size;
+        private int _capacity;
+        private int _head;
+        private int _tail;
+        private T[] _queueElements;
+
+        public CustomQueue()
+        {
+            _queueElements = new T[_defaultCapacity];
+            _head = 0;
+            _tail = 0;
+            _size = 0;
+            _capacity = _defaultCapacity;
+        }
+
+        public CustomQueue(int concreteCapacity)
+        {
+            if (concreteCapacity <= 0)
+                throw new ArgumentOutOfRangeException();
+            _queueElements = new T[concreteCapacity];
+            _head = 0;
+            _tail = 0;
+            _size = 0;
+            _capacity = concreteCapacity;
+        }
+
+        public CustomQueue(IEnumerable<T> itemCollection)
+        {
+            if (itemCollection.Count() == 0)
+            {
+                _queueElements = new T[_defaultCapacity];
+                _head = 0;
+                _tail = 0;
+                _size = 0;
+                _capacity = _defaultCapacity;
+            }
+            else
+            {
+                _queueElements = new T[itemCollection.Count() * 2];
+                Array.Copy(itemCollection.ToArray(), _queueElements, itemCollection.Count());
+                _head = 0;
+                _tail = itemCollection.Count();
+                _size = itemCollection.Count();
+                _capacity = itemCollection.Count()*2;
+            }
+        }
+
+        public int Count()
+        {
+            return _size;
+        }
+
+        public void Enqueue(T item)
+        {
+            if (_size > _capacity/2)
+                IncreaseCapacity();
+            _queueElements[_tail] = item;
+            _tail++;
+            _size++;
+        }
+
+        public T Dequeue()
+        {
+            if (_size == 0)
+                throw new InvalidOperationException();
+            var result = _queueElements[_head];
+            _head++;
+            _size--;
+            return result;
+        }
+
+        public T Peek()
+        {
+            if (_size == 0)
+                throw new InvalidOperationException();
+            return _queueElements[_head];
+        }
+
+        private void IncreaseCapacity()
+        {
+            var newQueue = new T[_capacity*2];
+            Array.Copy(_queueElements, _head, newQueue, 0, _size);
+            _capacity = _capacity*2;
+            _queueElements = newQueue;
+            _head = 0;
+            _tail = _size;
+        }
+
+        public void Clear()
+        {
+            Array.Clear(_queueElements, _head, _size);
+            _head = 0;
+            _tail = 0;
+            _size = 0;
+        }
+
+        public void CopyTo(T[] array, int startIndex)
+        {
+            for (int i = _head; i < _size; i++)
+            {
+                array[startIndex + i] = _queueElements[i];
+            }
+        }
+
+        public bool Contains(T item)
+        {
+            return _queueElements.Contains(item);
+        }
+
+        public T[] ToArray()
+        {
+            var newArray = new T[_size];
+            Array.Copy(_queueElements, _head, newArray, 0, _size);
+            return newArray;
+        }
+    }
+}
---
+++ 2017/Ilyashenko/Queue/UnitTestsForCustomQueue/UnitTest.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Queue;
+
+namespace UnitTestsForCustomQueue
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestCountAfterCreation()
+        {
+            var queue = new CustomQueue<int>();
+            Assert.AreEqual(0, queue.Count());
+        }
+
+        [TestMethod]
+        public void TestCreationWithIncorrectCapacity()
+        {
+            var actualException = new Exception();
+            var expectedException = new ArgumentOutOfRangeException();
+            try
+            {
+                var queue = new CustomQueue<int>(-1);
+            }
+            catch (Exception exception)
+            {
+                actualException = exception;
+            }
+            Assert.AreEqual(expectedException.GetType(), actualException.GetType());
+        }
+
+        [TestMethod]
+        public void TestCountAfterEnqueue()
+        {
+            var queue = new CustomQueue<int>();
+            for (int i = 0; i < 10; i++)
+                queue.Enqueue(i);
+            Assert.AreEqual(10, queue.Count());
+        }
+
+        [TestMethod]
+        public void TestCountAfterEnqueueAndDequeue()
+        {
+            var queue = new CustomQueue<int>();
+            for (int i = 0; i < 10; i++)
+                queue.Enqueue(i);
+            for (int i = 0; i < 4; i++)
+                queue.Dequeue();
+            Assert.AreEqual(6, queue.Count());
+        }
+
+        [TestMethod]
+        public void TestDequeueFromEmptyQueue()
+        {
+            var queue = new CustomQueue<int>();
+            var actualException = new Exception();
+            var expectedException = new InvalidOperationException();
+            try
+            {
+                queue.Dequeue();
+            }
+            catch (Exception exception)
+            {
+                actualException = exception;
+            }
+            Assert.AreEqual(expectedException.GetType(), actualException.GetType());
+        }
+
+        [TestMethod]
+        public void TestCountAfterClear()
+        {
+            var queue = new CustomQueue<int>();
+            for (int i = 0; i < 10; i++)
+                queue.Enqueue(i);
+            queue.Clear();
+            Assert.AreEqual(0, queue.Count());
+        }
+
+        [TestMethod]
+        public void TestCountAfterCreationWithArray()
+        {
+            var testArray = new int[] {0, 3, 5, 7, 9};
+            var queue = new CustomQueue<int>(testArray);
+            Assert.AreEqual(testArray.Length, queue.Count());
+        }
+
+        [TestMethod]
+        public void TestDequeueAfterCreationWithArray()
+        {
+            var testArray = new int[] { 0, 3, 5, 7, 9 };
+            var queue = new CustomQueue<int>(testArray);
+            Assert.AreEqual(testArray[0], queue.Dequeue());
+        }
+
+        [TestMethod]
+        public void TestPeek()
+        {
+            var testArray = new int[] { 0, 3, 5, 7, 9 };
+            var queue = new CustomQueue<int>(testArray);
+            Assert.AreEqual(testArray[0], queue.Peek());
+        }
+
+        [TestMethod]
+        public void TestQueueOnLargeDataAmount()
+        {
+            var queue = new CustomQueue<int>();
+            var array = new int[10000];
+            var arrayAsString = String.Empty;
+            var queueAsString = String.Empty;
+
+            for (int i = 0; i < 10000; i++)
+            {
+                queue.Enqueue(i);
+                array[i] = i;
+            }
+            foreach (var item in array)
+            {
+                arrayAsString += item;
+            }
+            while (queue.Count() > 0)
+            {
+                queueAsString += queue.Dequeue();
+            }
+            Assert.AreEqual(arrayAsString, queueAsString);
+        }
+
+        [TestMethod]
+        public void TestQueueToArray()
+        {
+            var queue = new CustomQueue<int>();
+            var array = new int[] {0, 3, 5, 7, 9, 11};
+            var arrayAsString = String.Empty;
+            var queueAsString = String.Empty;
+
+            foreach (var item in array)
+                queue.Enqueue(item);
+            var newArray = queue.ToArray();
+
+            foreach (var item in array)
+            {
+                arrayAsString += item;
+            }
+            foreach (var item in newArray)
+            {
+                queueAsString += item;
+            }
+            Assert.AreEqual(arrayAsString, queueAsString);
+        }
+    }
+}

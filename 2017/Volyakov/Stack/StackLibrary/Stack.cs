837f7e3914536702589ec49755b861fa4ddf1699
Volyakov Dmitriy <volyakovdima@yahoo.com>
2017-07-10 14:56:15 +0400

    Volyakov. Data structure. Stack

---
+++ 2017/Volyakov/Stack/StackLibrary/Stack.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace StackLibrary
+{
+    public class Stack<T>
+    {
+        private T[] _items;
+        private int _count;
+
+        public int Count
+        {
+            get
+            {
+                return _count;
+            }
+            private set
+            {
+                _count = value > 0 ? value : 0;
+            }
+        }
+        public int Capacity
+        {
+            get { return _items.Length; }
+        }
+
+        public Stack()
+        {
+            Init();
+        }
+        public Stack(IEnumerable<T> collection)
+        {
+            Init();
+            foreach (T item in collection)
+                Push(item);
+        }
+
+        private void Init()
+        {
+            _items = new T[8];
+            Count = 0;
+        }
+        private void CheckCapacity()
+        {
+            if(Count*3 > Capacity*2)
+            {
+                T[] newItems = new T[_items.Length * 2];
+                _items.CopyTo(newItems, 0);
+                _items = newItems;
+            }
+        }
+
+        public void Push(T pushedItem)
+        {
+            _items[Count] = pushedItem;
+            Count++;
+            CheckCapacity();
+        }
+        public T Pop()
+        {
+            if(Count!=0)
+            {
+                Count--;
+                return _items[Count];
+            }
+            throw new InvalidOperationException("The Stack is empty");
+        }
+        public T Peek()
+        {
+            if (Count != 0)
+                return _items[Count - 1];
+            throw new InvalidOperationException("The Stack is empty");
+        }
+        public bool Contains(T item)
+        {
+            return _items.Contains<T>(item);
+        }
+        public void Clear()
+        {
+            Init();
+        }
+    }
+}
---
+++ 2017/Volyakov/Stack/UnitTestProject/StackUnitTest.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StackLibrary;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest
+    {
+        private int[] _initArray = new int[] { 1, 2, 3, 4, 5 };
+
+        [TestMethod]
+        public void InitialCountEqualsZero()
+        {
+            Stack<int> stk = new Stack<int>();
+            Assert.AreEqual(stk.Count, 0);
+        }
+        [TestMethod]
+        public void InitWithArrayTest()
+        {
+            Stack<int> stk = new Stack<int>(_initArray);
+            Assert.AreEqual(stk.Count, _initArray.Length);
+        }
+        [TestMethod]
+        public void PushCountTest()
+        {
+            Stack<int> stk = new Stack<int>(_initArray);
+            int expectedCount = stk.Count + 1;
+            stk.Push(6);
+            Assert.AreEqual(stk.Count, expectedCount);
+        }
+        [TestMethod]
+        public void PopCountTest()
+        {
+            Stack<int> stk = new Stack<int>(_initArray);
+            int expectedCount = stk.Count - 1;
+            stk.Pop();
+            Assert.AreEqual(stk.Count, expectedCount);
+        }
+        [TestMethod]
+        public void PeekCountTest()
+        {
+            Stack<int> stk = new Stack<int>(_initArray);
+            int expectedCount = stk.Count;
+            stk.Peek();
+            Assert.AreEqual(stk.Count, expectedCount);
+        }
+        [TestMethod]
+        public void PushItemTest()
+        {
+            Stack<int> stk = new Stack<int>(_initArray);
+            int expectedItem = 6;
+            stk.Push(expectedItem);
+            int actualItem = stk.Pop();
+            Assert.AreEqual(actualItem, expectedItem);
+        }
+        [TestMethod]
+        public void PopItemTest()
+        {
+            Stack<int> stk = new Stack<int>(_initArray);
+            int firstItem = stk.Pop();
+            int secondItem = stk.Pop();
+            Assert.AreNotEqual(firstItem, secondItem);
+        }
+        [TestMethod]
+        public void PeekItemTest()
+        {
+            Stack<int> stk = new Stack<int>(_initArray);
+            int firstItem = stk.Peek();
+            int secondItem = stk.Peek();
+            Assert.AreEqual(firstItem, secondItem);
+        }
+        [TestMethod]
+        public void ContainsTest()
+        {
+            Stack<int> stk = new Stack<int>(_initArray);
+            int item = _initArray[0];
+            bool expectedResult = true;
+            Assert.AreEqual(stk.Contains(item), expectedResult);
+        }
+        [TestMethod]
+        public void ClearTest()
+        {
+            Stack<int> stk = new Stack<int>(_initArray);
+            stk.Clear();
+            Assert.AreEqual(stk.Count, 0);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void PopExceptionTest()
+        {
+            Stack<int> stk = new Stack<int>();
+            stk.Pop();
+        }
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void PeekExceptionTest()
+        {
+            Stack<int> stk = new Stack<int>();
+            stk.Peek();
+        }
+    }
+}

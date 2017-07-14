76477847c23e4d61cd359f06c46ef4584efef2b9
romarioGI <roma.fenc@yandex.ru>
2017-07-14 16:11:22 +0300

    List

---
+++ 2017/Gibadulin/List/myList.Tests/MyListTests.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace myList.Tests
+{
+    [TestClass]
+    public class MyListTests
+    {
+        [TestMethod]
+        public void List_initList_NotNull()
+        {
+            //arrange
+            List<object> list;
+            //act
+            list = new List<object>();
+            //assert
+            Assert.IsNotNull(list);
+        }
+
+        [TestMethod]
+        public void List_count_0()
+        {
+            //arrange
+            List<object> list;
+            //act
+            list = new List<object>();
+            //assert
+            Assert.IsTrue(list.Count==0);
+        }
+
+        [TestMethod]
+        public void Enumerate_initList_null() // test for exception of []
+        {
+            //arrange
+            List<object> list = new List<object>();
+            //act
+            object a = list[0];
+            //assert
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void PushBack_PushOneItem()
+        {
+            //arrange
+            List<object> list = new List<object>();
+            list.PushBack(5);
+            object expected = 5;
+            //act
+            object actual = list[0];
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void PushBack_PushTwoItem()
+        {
+            //arrange
+            List<object> list = new List<object>();
+            list.PushBack(1);
+            list.PushBack(2);
+            object expectedZero=1,
+                   expectedFirst = 2;
+            //act
+            object actualZero = list[0],
+                   actualFirst = list[1];
+            //assert
+            Assert.IsTrue(Equals(expectedZero,actualZero) && Equals(expectedFirst,actualFirst));
+        }
+
+        [TestMethod]
+        public void PushBack_PushTwoItem_CountEqual2()
+        {
+            //arrange
+            List<object> list = new List<object>();
+            list.PushBack(1);
+            list.PushBack(2);
+            int expected = 2;
+
+            //act
+            int actual = list.Count;
+
+            //assert
+            Assert.IsTrue(expected==list.Count);
+        }
+
+        [TestMethod]
+        public void Enumerate_ChangeOneItem()
+        {
+            //arrange
+            List<object> list = new List<object>();
+            list.PushBack(1);
+            object expected = 2;
+            //act
+            list[0] = 2;
+            object actual = list[0];
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Contains_PushAndSearchElement_True()
+        {
+            //arrange
+            List<object> list = new List<object>();
+            list.PushBack(0);
+            //act
+            bool actual = list.Contains(0);
+            //assert
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void Contains_PushAndSearchDifferentElements_False()
+        {
+            //arrange
+            List<object> list = new List<object>();
+            list.PushBack(0);
+            //act
+            bool actual = list.Contains(1);
+            //assert
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod]
+        public void IndexOfFirstEquals_WithoutEqual()
+        {
+            //arrange
+            List<object> list = new List<object>();
+            list.PushBack(0);
+            list.PushBack(1);
+            list.PushBack(2);
+            int expected = -1;
+            //act
+            int actual = list.IndexOfFirstEquals(3);
+            //assert
+            Assert.IsTrue(actual == expected);
+        }
+
+        [TestMethod]
+        public void IndexOfFirstEquals_TwotEqual()
+        {
+            //arrange
+            List<object> list = new List<object>();
+            list.PushBack(0);
+            list.PushBack(1);
+            list.PushBack(0);
+            int expected = 0;
+            //act
+            int actual = list.IndexOfFirstEquals(0);
+            //assert
+            Assert.IsTrue(actual == expected);
+        }
+
+        [TestMethod]
+        public void Insert_IndexMoreCounts() // test for exception
+        {
+            //arrange
+            List<object> list = new List<object>();
+            list.PushBack(0);
+            //act
+            list.Insert(3, 3);
+            //assert
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void Insert_AppendFirst()
+        {
+            //arrange
+            List<object> list = new List<object>();
+            list.PushBack(0);
+            object expected = 1;
+            //act
+            list.Insert(1, 0);
+            object actual = list[0];
+            //assert
+            Assert.AreEqual(expected,actual);
+        }
+
+        [TestMethod]
+        public void Insert_AppendBack()
+        {
+            //arrange
+            List<object> list = new List<object>();
+            list.PushBack(0);
+            object expected = 1;
+            //act
+            list.Insert(1, 1);
+            object actual = list[1];
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void RemoveFirstEquals_RemoveFirstItem()
+        {
+            //arrange
+            List<object> list = new List<object>();
+            list.PushBack(0);
+            list.PushBack(1);
+            object expectedItem = 1;
+            int expectedCount = 1;
+            //act
+            list.RemoveFirstEquals(0);
+            object actualItem = list[0];
+            int actualCount = list.Count;
+            //assert
+            Assert.IsTrue(expectedCount==actualCount && Equals(expectedItem,actualItem));
+        }
+    }
+}
---
+++ 2017/Gibadulin/List/myList/List.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace myList
+{
+    public class List<T>
+    {
+        class Node
+        {
+            public T info;
+            public Node next;
+            public Node(T Info)
+            {
+                info = Info;
+                next = null;
+            }
+        }
+
+        private Node head,
+           tail;
+        private int count;
+
+        public List()
+        {
+            head = null;
+            tail = null;
+            count = 0;
+        }
+
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index >= count)
+                {
+                    throw new Exception("Index out of range");
+                }
+                Node current = head;
+                for (int i = 0; i < index; i++)
+                    current = current.next;
+                return current.info;
+            }
+            set
+            {
+                if (index >= count)
+                {
+                    throw new Exception("Index out of range");
+                }
+                Node current = head;
+                for (int i = 0; i < index; i++)
+                    current = current.next;
+                current.info = value;
+            }
+        }
+
+        public void PushBack(T item)
+        {
+            if (head == null)
+            {
+                head = new Node(item);
+                tail = head;
+            }
+            else
+            {
+                tail.next = new Node(item);
+                tail = tail.next;
+            }
+            count++;
+        }
+
+        public bool Contains(T item)
+        {
+            Node current = head;
+            while (current != null && !current.info.Equals(item))
+                current = current.next;
+            if (current == null)
+                return false;
+            return true;
+        }
+
+        public int IndexOfFirstEquals(T item)
+        {
+            Node current = head;
+            int index = 0;
+            while (current != null && !current.info.Equals(item))
+            {
+                current = current.next;
+                index++;
+            }
+            if (current == null)
+                return -1;
+            return index;
+        }
+
+        public void Insert(T item, int index)
+        {
+            if (index > count)
+            {
+                throw new Exception("Index out of range");
+            }
+            if (index == count)
+            {
+                PushBack(item);
+                return;
+            }
+            if (index == 0)
+            {
+                Node current = new Node(item);
+                current.next = head;
+                head = current;
+                count++;
+            }
+            else
+            {
+                Node previous = head;
+                for (int i = 0; i < index - 1; i++)
+                    previous = previous.next;
+                Node current = new Node(item);
+                current.next = previous.next;
+                previous.next = current.next;
+                count++;
+            }
+        }
+
+        public void RemoveFirstEquals(T item)
+        {
+            if (head.info.Equals(item))
+            {
+                head = head.next;
+                count--;
+            }
+            else
+            {
+                Node current = head;
+                while (current.next != null && !current.next.info.Equals(item))
+                    current = current.next;
+                if (current.next != null)
+                {
+                    current.next = current.next.next;
+                    count--;
+                    if (current.next == null)
+                        tail = current;
+                }
+            }
+        }
+
+        public void Clear()
+        {
+            head = null;
+            tail = null;
+            count = 0;
+        }
+    }
+}

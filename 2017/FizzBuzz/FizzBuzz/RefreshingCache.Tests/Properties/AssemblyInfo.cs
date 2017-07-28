17d1eb3e8b19d5f5cb910b53db1903bbad136f0b
Sergey Petrov <sergeyp@confirmit.com>
2017-07-28 17:15:48 +0300

    FizzBuzz

---
+++ 2017/FizzBuzz/FizzBuzz/RefreshingCache.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RefreshingCache.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RefreshingCache.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2275845a-be2c-483b-abfb-57d73b708b36")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ 2017/FizzBuzz/FizzBuzz/RefreshingCache/FizzBuzz.cs
@@ -0,0 +1,36 @@
+﻿namespace RefreshingCache
+{
+    public class FizzBuzz
+    {
+        private const string Fizz = "Fizz";
+        private const string Buzz = "Buzz";
+
+        public string Process(int number)
+        {
+            if (number.IsDividableBy(15))
+            {
+                return Fizz + Buzz;
+            }
+
+            if (number.IsDividableBy(5))
+            {
+                return Buzz;
+            }
+
+            if (number.IsDividableBy(3))
+            {
+                return Fizz;
+            }
+
+            return number.ToString();
+        }
+    }
+
+    public static class Int32Extensions
+    {
+        public static  bool IsDividableBy(this int number, int divider)
+        {
+            return number % divider == 0;
+        }
+    }
+}

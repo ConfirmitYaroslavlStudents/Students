76c873e4804f72ed886635ab294d81604a57c95d
Fenixton <Fenixton@users.noreply.github.com>
2017-07-29 20:39:21 +0300

    Ilyashenko. FizzBuzz.

---
+++ 2017/Ilyashenko/FizzBuzz/FizzBuzz.Tests/FizzBuzzTests.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using FizzBuzzLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzzTests
+{
+    [TestClass]
+    public class FizzBuzzTests
+    {
+        private FizzBuzz _fizzBuzz;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _fizzBuzz = new FizzBuzz();
+        }
+
+        [TestMethod]
+        public void GetCorrectStringForNumberOne()
+        {
+            var numberAsString = _fizzBuzz.Process(1);
+
+            Assert.AreEqual("1", numberAsString);
+        }
+
+        [TestMethod]
+        public void GetCorrectStringForNumberTwo()
+        {
+            var numberAsString = _fizzBuzz.Process(2);
+
+            Assert.AreEqual("2", numberAsString);
+        }
+
+        [TestMethod]
+        public void GetCorrectStringForNumberFive()
+        {
+            var numberAsString = _fizzBuzz.Process(5);
+
+            Assert.AreEqual("Buzz", numberAsString);
+        }
+
+        [TestMethod]
+        public void GetCorrectStringForNumberTen()
+        {
+            var numberAsString = _fizzBuzz.Process(10);
+
+            Assert.AreEqual("Buzz", numberAsString);
+        }
+
+        [TestMethod]
+        public void GetCorrectStringForNumberThree()
+        {
+            var numberAsString = _fizzBuzz.Process(3);
+
+            Assert.AreEqual("Fizz", numberAsString);
+        }
+
+        [TestMethod]
+        public void GetCorrectStringForNumberFifteen()
+        {
+            var numberAsString = _fizzBuzz.Process(15);
+
+            Assert.AreEqual("FizzBuzz", numberAsString);
+        }
+
+        [TestMethod]
+        public void GetCorrectStringForNewRuleAddedViaConstructor()
+        {
+            _fizzBuzz = new FizzBuzz(new string[] { "2 Banana" });
+
+            var numberAsString = _fizzBuzz.Process(2);
+
+            Assert.AreEqual("Banana", numberAsString);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FormatException))]
+        public void AddRuleInWrongFormatViaConstructior()
+        {
+            _fizzBuzz = new FizzBuzz(new string[] { "2-Banana" });
+        }
+
+        [TestMethod]
+        public void AddNewRuleViaMethod()
+        {
+            _fizzBuzz.AddRule(2, "Banana");
+
+            var numberAsString = _fizzBuzz.Process(2);
+
+            Assert.AreEqual("Banana", numberAsString);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void AddExistingRuleViaMethod()
+        {
+            _fizzBuzz.AddRule(2, "Banana");
+            _fizzBuzz.AddRule(2, "Banana");
+
+            var numberAsString = _fizzBuzz.Process(2);
+
+            Assert.AreEqual("Banana", numberAsString);
+        }
+
+        [TestMethod]
+        public void RemoveRuleViaMethod()
+        {
+            _fizzBuzz.RemoveRule(3);
+
+            var numberAsString = _fizzBuzz.Process(3);
+
+            Assert.AreEqual("3", numberAsString);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void RemoveUnexistingRule()
+        {
+            _fizzBuzz.RemoveRule(2);
+        }
+
+        [TestMethod]
+        public void ChangeRule()
+        {
+            _fizzBuzz.ChangeRule(3, "Fidz");
+
+            var numberAsString = _fizzBuzz.Process(3);
+
+            Assert.AreEqual("Fidz", numberAsString);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ChangeUnexistingRule()
+        {
+            _fizzBuzz.ChangeRule(2, "Zzap");
+        }
+    }
+}
---
+++ 2017/Ilyashenko/FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using FizzBuzzLib;
+
+namespace FizzBuzzApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fizzBuzz = new FizzBuzz(new string[] { "11 Banana", "17 Orange"});
+            fizzBuzz.AddRule(2, "Hello");
+            fizzBuzz.AddRule(7, "World");
+
+            for (int i = 1; i <= 100; i++)
+                Console.WriteLine(fizzBuzz.Process(i));
+
+            Console.ReadKey();
+        }
+    }
+}

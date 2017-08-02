1ba43f2b9d95cd4c38a01ca62f6195b764a0637b
romarioGI <roma.fenc@yandex.ru>
2017-08-02 23:17:00 +0300

    FizzBuzz

---
+++ 2017/Gibadulin/ExampleTDD/ExampleTDD/BananaFizzBuzz.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+
+namespace ExampleTDD
+{
+    public class BananaFizzBuzz
+    {
+        private const string Banana = "Banana";
+        private const string Fizz = "Fizz";
+        private const string Buzz = "Buzz";
+
+        public string Process(int number)
+        {
+            var result = new StringBuilder();
+
+            if (number.IsDividableBy(2))
+                result.Append(Banana);
+
+            if (number.IsDividableBy(3))
+                result.Append(Fizz);
+
+            if (number.IsDividableBy(5))
+                result.Append(Buzz);
+
+            if (result.Length == 0)
+                result.Append(number.ToString());
+
+            return result.ToString();
+        }
+    }
+
+    public static class Int32Extensions
+    {
+        public static bool IsDividableBy(this int number, int divider)
+        {
+            return number % divider == 0;
+        }
+    }
+}

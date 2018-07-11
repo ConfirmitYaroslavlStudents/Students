8d053f1a56b8bc750b0a319cc27729e9e1ae355e
dkblock <wadeouter@gmail.com>
2018-07-11 23:22:36 +0300

    Added MyDictionary realization

---
+++ 2018/Kulikov/MyDictionary/MyDictionary/MyDictionary.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    public class Node<TKey, TValue>
+    {
+        public Node(TKey key, TValue value)
+        {
+            Key = key;
+            Value = value;
+        }
+        public TKey Key { get; set; }
+        public TValue Value { get; set; }
+        public Node<TKey, TValue> Next { get; set; }
+    }
+    public class MyDictionary<TKey, TValue>
+    {
+        Node<TKey, TValue> first;
+        Node<TKey, TValue> last;
+        int count;
+
+        public MyDictionary()
+        {
+            first = null;
+            last = null;
+            count = 0;
+        }
+        public void Add(TKey key, TValue value)
+        {
+            Node<TKey, TValue> node = new Node<TKey, TValue>(key, value);
+
+            if (first == null)
+            {
+                first = node;
+            }
+            else
+            {
+                var current = first;
+
+                while (current != null)
+                {
+                    if (!current.Key.Equals(node.Key))
+                    {
+                        current = current.Next;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                if (current == null)
+                {
+                    last.Next = node;
+                }
+                else
+                {
+                    throw new ArgumentException("Элемент с тем же ключом уже был добавлен.");
+                }
+            }
+
+            last = node;
+            count++;
+
+        }
+
+        public bool Remove(TKey key)
+        {
+            Node<TKey, TValue> current = first;
+            Node<TKey, TValue> previous = null;
+
+            while (current != null)
+            {
+                if (current.Key.Equals(key))
+                {
+                    if (previous != null)
+                    {
+                        previous.Next = current.Next;
+                        if (current.Next == null)
+                        {
+                            last = previous;
+                        }
+                    }
+                    else
+                    {
+                        first = first.Next;
+
+                        if (first == null)
+                        {
+                            last = null;
+                        }
+                    }
+                    count--;
+                    return true;
+                }
+                previous = current;
+                current = current.Next;
+            }
+            return false;
+        }
+
+        public bool ContainsKey(TKey key)
+        {
+            var current = first;
+            while (current != null)
+            {
+                if (current.Key.Equals(key))
+                {
+                    return true;
+                }
+                current = current.Next;
+            }
+            return false;
+        }
+
+        public int Count
+        {
+            get { return count; }
+        }
+
+        private Node<TKey, TValue> NodeByKey(TKey key)
+        {
+            var current = first;
+            while (current != null)
+            {
+                if (current.Key.Equals(key))
+                {
+                    return current;
+                }
+                current = current.Next;
+            }
+            throw new KeyNotFoundException();
+        }
+
+        public TValue this[TKey key]
+        {
+            get { return NodeByKey(key).Value; }
+            set { NodeByKey(key).Value = value; }
+        }
+
+    }
+}
---
+++ 2018/Kulikov/MyDictionary/MyDictionaryTest/MyDictionaryTests.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyDictionary;
+
+namespace MyDictionaryTest
+{
+    [TestClass]
+    public class MyDictionaryTests
+    {
+        [TestMethod]
+        public void Add_4_objects()
+        {
+            MyDictionary<int, char> MD = new MyDictionary<int, char>();
+            MD.Add(1, 'a');
+            MD.Add(2, 'b');
+            MD.Add(3, 'c');
+            MD.Add(4, 'd');
+
+            int actual = MD.Count;
+            int expected = 4;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Remove_2_objects()
+        {
+            MyDictionary<int, char> MD = new MyDictionary<int, char>();
+            MD.Add(1, 'a');
+            MD.Add(2, 'b');
+            MD.Add(3, 'c');
+            MD.Add(4, 'd');
+
+            MD.Remove(1);
+            MD.Remove(4);
+
+            int actual = MD.Count;
+            int expected = 2;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Contains_3()
+        {
+            MyDictionary<int, char> MD = new MyDictionary<int, char>();
+            MD.Add(1, 'a');
+            MD.Add(2, 'b');
+            MD.Add(3, 'c');
+            MD.Add(4, 'd');
+
+            bool actual = MD.ContainsKey(3);
+            bool expected = true;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void NotContains_10()
+        {
+            MyDictionary<int, char> MD = new MyDictionary<int, char>();
+            MD.Add(1, 'a');
+            MD.Add(2, 'b');
+            MD.Add(3, 'c');
+            MD.Add(4, 'd');
+
+            bool actual = MD.ContainsKey(10);
+            bool expected = false;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetValue_ByKey()
+        {
+            MyDictionary<int, char> MD = new MyDictionary<int, char>();
+            MD.Add(1, 'a');
+            MD.Add(2, 'b');
+            MD.Add(3, 'c');
+            MD.Add(4, 'd');
+
+            char actual = MD[4];
+            char expected = 'd';
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Add_ExistingKey()
+        {
+            MyDictionary<int, char> MD = new MyDictionary<int, char>();
+            MD.Add(1, 'a');
+            MD.Add(2, 'b');
+            MD.Add(3, 'c');
+            MD.Add(3, 'd');
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(KeyNotFoundException))]
+        public void GetValue_ByNotExistingKey()
+        {
+            MyDictionary<int, char> MD = new MyDictionary<int, char>();
+            MD.Add(1, 'a');
+            MD.Add(2, 'b');
+            MD.Add(3, 'c');
+            MD.Add(4, 'd');
+
+            char actual = MD[5];
+        }
+
+        [TestMethod]
+        public void ChangeValue_ByKey()
+        {
+            MyDictionary<int, char> MD = new MyDictionary<int, char>();
+            MD.Add(1, 'a');
+            MD.Add(2, 'b');
+            MD.Add(3, 'c');
+            MD.Add(4, 'd');
+
+            MD[4] = 'x';
+
+            char actual = MD[4];
+            char expected = 'x';
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(KeyNotFoundException))]
+        public void RemoveItem_and_GetValue()
+        {
+            MyDictionary<int, char> MD = new MyDictionary<int, char>();
+            MD.Add(1, 'a');
+            MD.Add(2, 'b');
+            MD.Add(3, 'c');
+            MD.Add(4, 'd');
+            MD.Remove(1);
+
+            char actual = MD[1];
+        }
+
+        [TestMethod]
+        public void NotContains_RemovedItem()
+        {
+            MyDictionary<int, char> MD = new MyDictionary<int, char>();
+            MD.Add(1, 'a');
+            MD.Add(2, 'b');
+            MD.Add(3, 'c');
+            MD.Add(4, 'd');
+
+            MD.Remove(2);
+
+            bool actual = MD.ContainsKey(2);
+            bool expected = false;
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

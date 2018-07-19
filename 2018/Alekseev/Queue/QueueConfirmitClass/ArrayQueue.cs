c4a0cd12075b61a2fbc2f5b4bce63c216a488a22
DaniilAlekseev <danaleks98@gmail.com>
2018-07-19 12:30:51 +0300

    adding Array implementation of Queue and XUnit tests for both variations of Queue

---
+++ 2018/Alekseev/Queue/QueueConfirmitClass/ArrayQueue.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueConfirmitClass
+{
+    public class ArrayQueue<T> where T : IComparable<T>
+    {
+        int MAX_SIZE = 20;
+        T[] _queue;
+        int front ;
+        int rear;
+
+        public ArrayQueue()
+        {
+            _queue = new T[MAX_SIZE];
+            front = rear = -1;
+        }
+        public void Enqueue(T element)
+        {
+            if (IsFull())
+            {
+                Console.WriteLine("Queue is full!");
+                return;
+            }
+
+            if (IsEmpty())
+            {
+                front = rear = 0;
+            }
+            else
+                rear = (rear + 1) % MAX_SIZE;
+
+            _queue[rear] = element;
+        }
+
+        public bool IsFull()
+        {
+            return ((rear + 1) % MAX_SIZE == front) ? true : false;
+        }
+        public bool IsEmpty()
+        {
+            return (front == -1 && rear == -1);
+        }
+
+        public void Dequeue()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Queue is empty!");
+                return;
+            }
+            if (rear == front)
+                rear = front = -1;
+            else
+                front = (front + 1) % MAX_SIZE;
+        }
+
+        public T Peek()
+        {
+            if(front == -1)
+            {
+                Console.WriteLine("Queue is empty. Have no element to peek");
+                return default(T);
+            }
+            return _queue[front];
+        }
+
+        public void Print()
+        {
+            int count = Count(); 
+            Console.WriteLine("Printing queue...");
+            for(int i = 0; i < count; i++)
+            {
+                int index = (front + i) % MAX_SIZE;
+                Console.Write("{0} ", _queue[i]);
+            }
+            Console.WriteLine();
+        }
+
+        public void Clear()
+        {
+            front = rear = -1;
+        }
+
+        public int Count()
+        {
+            if (rear == -1 && front == -1)
+                return (rear + MAX_SIZE - front) % MAX_SIZE;
+            return (rear + MAX_SIZE - front) % MAX_SIZE + 1;
+        }
+
+        public bool Contains(T element)
+        {
+            return Array.Exists(_queue, elem => elem.CompareTo(element) == 0);
+        }
+    }
+}
---
+++ 2018/Alekseev/Queue/UnitTests/Tests.cs
@@ -0,0 +1,110 @@
+﻿using Xunit;
+using QueueConfirmitClass;
+
+namespace UnitTests
+{
+    public class Tests
+    {
+        ArrayQueue<int> _arrayQueue = new ArrayQueue<int>();
+        ListQueue<int> _listQueue = new ListQueue<int>();
+        [Fact]
+        public void CheckContainsArrayQueue()
+        {
+            AddToArrayQueue(5);
+            Assert.True(_arrayQueue.Contains(1));
+        }
+
+        [Fact]
+        public void CheckContainsListQueue()
+        {
+            AddToListQueue(5);
+            Assert.True(_listQueue.Contains(1));
+        }
+
+        public void AddToArrayQueue(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                _arrayQueue.Enqueue(i + 1);
+            }
+        }
+
+        public void AddToListQueue(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                _listQueue.Enqueue(i + 1);
+            }
+        }
+
+        [Fact]
+        public void CheckCountArrayQueue()
+        {
+            AddToArrayQueue(5);
+            Assert.Equal(5,_arrayQueue.Count());
+        }
+
+        [Fact]
+        public void CheckCountListQueue()
+        {
+            AddToListQueue(5);
+            Assert.Equal(5, _listQueue.Count());
+        }
+
+        [Fact]
+        public void CheckPeekArrayQueue()
+        {
+            AddToArrayQueue(5);
+            Assert.Equal(1,_arrayQueue.Peek());
+        }
+
+        [Fact]
+        public void CheckPeekListQueue()
+        {
+            AddToListQueue(5);
+            Assert.Equal(1,_listQueue.Peek());
+        }
+
+        [Fact]
+        public void CheckDequeueArrayQueue()
+        {
+            AddToArrayQueue(5);
+            _arrayQueue.Dequeue();
+            Assert.Equal(2,_arrayQueue.Peek());
+        }
+
+        [Fact]
+        public void CheckDequeueListQueue()
+        {
+            AddToListQueue(5);
+            _listQueue.Dequeue();
+            Assert.Equal(2,_listQueue.Peek());
+        }
+
+        [Fact]
+        public void CheckFullArrayQueue()
+        {
+            AddToArrayQueue(5);
+            for (int i = 5; i < 21; i++)
+                _arrayQueue.Enqueue(i);
+            _arrayQueue.Enqueue(22);
+            Assert.Equal(20,_arrayQueue.Count());
+        }
+
+        [Fact]
+        public void CheckClearArrayQueue()
+        {
+            AddToArrayQueue(5);
+            _arrayQueue.Clear();
+            Assert.Equal(0,_arrayQueue.Count());
+        }
+
+        [Fact]
+        public void CheckClearListQueue()
+        {
+            AddToListQueue(5);
+            _listQueue.Clear();
+            Assert.Equal(0,_listQueue.Count());
+        }
+    }
+}

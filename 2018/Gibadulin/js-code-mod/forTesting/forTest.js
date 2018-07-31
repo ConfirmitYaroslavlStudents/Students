6be028004c256e45034fd63b17f977c457f0b0cc
romarioGI <roma.fenc@yandex.ru>
2018-07-31 22:57:47 +0300

    edits
    The function of reading options is implemented. Optimization of functions working with arguments. Code cleaning.

---
+++ 2018/Gibadulin/js-code-mod/forTesting/forTest.js
@@ -0,0 +1,17 @@
+/*
+options:
+
+show('p1','p2');
+show.must('p1').go('p2').on(p3);
+
+*/
+
+show();
+var x = 10;
+show(x, y);
+show(fr, x == 10);
+function functionName() {
+  show(fr, x == 10);
+}
+w(show(q, e));
+show(qwerty, ytrewq);
--- 2018/Gibadulin/js-code-mod/transforms/change-call-function-style.js
+++ 2018/Gibadulin/js-code-mod/transforms/change-call-function-style.js
@@ -1,76 +1,76 @@
-module.exports = function(fileInfo, api, options) {
+module.exports = function (fileInfo, api, options) {
   getOptions(options);
 
-  const j            = api.jscodeshift;
-  const root         = j(fileInfo.source);
-  const codeNodes    = getStartNodes(root, j, fileInfo.path);
-  const oldCallNodes = getStartNodes(j(options.old), j, 'options --old')[0];
-  const newCallNodes = getStartNodes(j(options.new), j, 'options --new')[0];
+  const j                   = api.jscodeshift;
+  const root                = getAST(fileInfo.source, j, fileInfo.path);
+  const codeNodes           = getBodyNodes(root, j);
+  const oldCallNode         = getBodyNodes(getAST(options.old, j, "option --old"), j)[0];
+  const newCallExpressions  = getAST(options.new, j, 'option --new').find(j.CallExpression);
 
-  for (let x in codeNodes){
-    args = new Object();
-    if(equalNodes(oldCallNodes,codeNodes[x])){
-      //модифицировать newCallNodes под нужные аргументы
-      let q = j(options.new).find(j.CallExpression).forEach(function (p) {
-        let nodes = p.node.arguments;
-        for(let a in nodes)
-          if(nodes[a].type=='Literal'){
-            if(args[nodes[a].value] === undefined)
-              throw new Error("Uncorrect argumets in options");
-            nodes[a]=args[nodes[a].value];
-          }
-      }).toSource();
-
-      codeNodes[x] = getStartNodes(j(q), j, 'options --new')[0];
-      // вернуть в изначально состояние newCallNodes желательно не создавая заново
+  for (let x in codeNodes) {
+    const isEqual = equalNodes(oldCallNode, codeNodes[x]);
+    if (isEqual) {
+      let startPos       = { value: 0 };
+      let newCallArgs    = [];
+      let changedNewCall = newCallExpressions.forEach(p => substituteArguments(p, newCallArgs)).toSource();
+      codeNodes[x]       = getBodyNodes(j(changedNewCall), j)[0];
+      newCallExpressions.forEach(p => retrieveArguments(p, newCallArgs, startPos));
     }
   }
 
   return root.toSource();
-  //return null;
 };
 
 function getOptions(options) {
-  // считываем из файла и заносим в переменную options
-  // костыль из-за не переносимости nomnom-ом строк
-  options.old = "show('p1','p2');\n";
-  options.new = "show.must('p1').go('p2').on(p3);\n";
+  const fs = require('fs');
+
+  let text = fs.readFileSync(options.optPath, 'utf8').split('\n');
+
+  options.old = text[0];
+  options.new = text[1];
 }
 
-function getStartNodes(ast, j, codeName) {
+function getAST(code, j, codeName) {
   try {
-    return ast.find(j.Program).get('body').value;
+    return j(code);
   }
   catch (ex) {
-    if(codeName !== undefined)
+    if (codeName !== undefined)
       ex.message += ` in ${codeName}`;
     throw ex;
   }
 }
 
+function getBodyNodes(ast, j) {
+    return ast.find(j.Program).get('body').value;
+}
+
 function equalNodes(nodeA, nodeB) {
   if(nodeA.type !== nodeB.type || typeComparator[nodeA.type] === undefined)
     return false;
-  return typeComparator[nodeA.type](nodeA,nodeB);
+  return typeComparator[nodeA.type](nodeA, nodeB);
 }
 
 const typeComparator = new Object();
-let   args           = new Object();
+const oldCallArgs    = new Object();
 
 typeComparator['ExpressionStatement'] = function (nodeA, nodeB) {
-  return equalNodes(nodeA.expression,nodeB.expression);
+  return equalNodes(nodeA.expression, nodeB.expression);
 }
 
 typeComparator['CallExpression'] = function (nodeA, nodeB) {
   return typeComparator['arguments'](nodeA.arguments, nodeB.arguments) && equalNodes(nodeA.callee, nodeB.callee);
 }
 
-typeComparator['arguments'] = function (nodeA, nodeB){
-  if(nodeA.length !== nodeB.length)
+typeComparator['arguments'] = function (nodeA, nodeB) {
+  if (nodeA.length !== nodeB.length)
     return false;
-  for (let i=0;i<nodeA.length;i++)
-    if(nodeA[i].type == 'Literal') args[nodeA[i].value] = nodeB[i];
+  for (let i = 0; i < nodeA.length; i++) {
+    if(nodeA[i].type == 'Literal') {
+       oldCallArgs[nodeA[i].value] = nodeB[i];
+    }
     else if(!equalNodes(nodeA[i], nodeB[i])) return false;
+  }
   return true;
 }
 
@@ -80,7 +80,7 @@ typeComparator['Identifier'] = function (nodeA, nodeB) {
 
 typeComparator['MemberExpression'] = function (nodeA, nodeB) {
   if (nodeA.computed === nodeB.computed)
-    return equalNodes(nodeA.object,nodeB.object) && equalNodes(nodeA.property, nodeB.property);
+    return equalNodes(nodeA.object, nodeB.object) && equalNodes(nodeA.property, nodeB.property);
   return false;
 }
 
@@ -88,6 +88,30 @@ typeComparator['Literal'] = function (nodeA, nodeB) {
   return nodeA.value === nodeB.value;
 }
 
-function setArguments(node) {
+function substituteArguments(callExpressionPath, previousArgs) {
+  const callExpressionArgs = callExpressionPath.node.arguments;
+  for(let i in callExpressionArgs) {
+    if(callExpressionArgs[i].type !== 'Literal') {
+      previousArgs.push(undefined);
+      continue;
+    }
+    const replacingArgument = oldCallArgs[callExpressionArgs[i].value];
+    if(replacingArgument === undefined){
+      throw new Error(`The argument ${callExpressionArgs[i].value} is not found`);
+    }
+    previousArgs.push(callExpressionArgs[i]);
+    callExpressionArgs[i] = replacingArgument;
+  }
+}
 
+function retrieveArguments(callExpressionPath, args, startPos) {
+  let additive = 0;
+  const callExpressionArgs = callExpressionPath.node.arguments;
+  for(let i in callExpressionArgs) {
+    additive++;
+    if (args[Number(i)+startPos.value] === undefined)
+      continue;
+    callExpressionArgs[i] = args[Number(i)+startPos.value];
+  }
+  startPos.value+=additive;
 }

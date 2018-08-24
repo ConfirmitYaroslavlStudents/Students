37c5c152457cf29191ad1667d382ae61da4010da
Kirill Vinogradov <kirill.vinogradov@confirmit.com>
2018-08-24 14:06:18 +0300

    Remove unnecessary "using"s;

--- 2018/Pichkur/Championship/WindowsFormsUserInterface/FormDataInputWorker.cs
+++ 2018/Pichkur/Championship/WindowsFormsUserInterface/FormDataInputWorker.cs
@@ -1,10 +1,6 @@
-﻿using Championship;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using System.Windows.Forms;
+using ChampionshipLibrary;
 
 namespace WindowsFormsUserInterface
 {

b3b5155687086cdba8655ebd7a531775340eeada
Furmanets <furmanets_vit@mail.ru>
2018-08-26 18:48:54 +0300

    Creating project, studying pattern decorator;

---
+++ 2018/Furmanets/PrintText/PrintText/Beep.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PrintText
+{
+    class Beep:IPrintModification
+    {
+        private IPrintModification _printModification;
+        public Beep(IPrintModification printModification)
+        {
+            _printModification = printModification;
+        }
+
+        public void Do(string text)
+        {
+            var textSplit = text.Split(' ');
+            for (int i = 0; i < textSplit.Length; i++)
+            {
+                Console.Beep(300, 261);
+            }
+            _printModification.Do(text);
+        }
+    }
+}

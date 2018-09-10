a539c765648fe501c953955981e4ac6bf7de8cc7
Dmitry Banokin <dmitry.banokin@gmail.com>
2018-09-10 16:30:00 +0300

    CandidateAccounting. Added server side rendered styled componets for page placeholder. Refactoring.

--- CandidatesAccounting/server/template.js
+++ CandidatesAccounting/server/template.js
@@ -1,38 +1,41 @@
 import path from 'path'
+import React from 'react'
+import ReactDOMServer from 'react-dom/server'
+import { ServerStyleSheet } from 'styled-components'
+import PagePlaceholder from './components/pagePlaceholder'
 
 const template = (props) => {
   const { assetsRoot, username } = props
 
   const config = { username }
 
-  //todo: https://www.styled-components.com/docs/advanced#server-side-rendering
+  const sheet = new ServerStyleSheet()
+  const pagePlaceholder = ReactDOMServer.renderToString(sheet.collectStyles(<PagePlaceholder />))
+  const styles = sheet.getStyleTags()
 
   return `
     <!doctype html>
     <html lang="ru" class="html">
-        <head>
-            <meta charset="utf-8">
-            <meta name="theme-color" content="#3F51B5">
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-            <title>Candidate Accounting</title>
-            <link rel="manifest" href="${path.join(assetsRoot, 'manifest.json')}">
-            <link rel="icon" href="${path.join(assetsRoot, 'favicon.ico')}" type= "image/x-icon" >
-            <link rel="shortcut icon" href="${path.join(assetsRoot, 'favicon.ico')}" type="image/x-icon" >
-        </head>
-        <body style="background-color: #CCC;">
-            <div id="root">
-                <div style="position: fixed; top: 0; left: 0; width: 100%; height: 60px; background-color: #3F51B5; z-index: 10;"></div>                
-                <div style="position: fixed; top: 60px; left: 0; width: 100%; height: 48px; background-color: #f5f5f5; box-shadow: 0 0 4px 4px rgba(0,0,0,0.2);" ></div>
-                <div style="margin: -10px; font-size: 190%; font-weight: bold; color: #888; font-family: sans-serif; text-align: center; padding-top: 185px;">
-                    CandidateAccounting is loading...
-                </div>
-            </div>
-            <script type="text/javascript">
-              window['APP_CONFIG'] = ${JSON.stringify(config)}
-            </script>
-            <script async type="text/javascript" src="${path.join(assetsRoot, 'vendors.js')}"></script>
-            <script async type="text/javascript" src="${path.join(assetsRoot, 'main.js')}"></script>
-        </body>
+      <head>
+        <meta charset="utf-8">
+        <meta name="theme-color" content="#3F51B5">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Candidate Accounting</title>
+        <link rel="manifest" href="${path.join(assetsRoot, 'manifest.json')}">
+        <link rel="icon" href="${path.join(assetsRoot, 'favicon.ico')}" type= "image/x-icon" >
+        <link rel="shortcut icon" href="${path.join(assetsRoot, 'favicon.ico')}" type="image/x-icon" >
+        ${styles}
+      </head>
+      <body>
+        <div id="root">
+            ${pagePlaceholder}  
+        </div>                      
+        <script type="text/javascript">
+          window['APP_CONFIG'] = ${JSON.stringify(config)}
+        </script>
+        <script async type="text/javascript" src="${path.join(assetsRoot, 'vendors.js')}"></script>
+        <script async type="text/javascript" src="${path.join(assetsRoot, 'main.js')}"></script>
+      </body>
     </html>`
 }
 

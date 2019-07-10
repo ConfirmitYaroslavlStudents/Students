a22a5b3fbe1711afaff789fccc8bf61143ddba1b
Akane-san <daryayar@yandex.ru>
2019-07-10 23:52:25 +0300

    First Task Queue

---
+++ 2019/Grebenchuk/QueueClass/QueueClass/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UsualQueue<int> queue1 = new UsualQueue<int>();
+            Console.WriteLine(queue1.Count);
+
+            //Console.WriteLine(queue1.Dequeue());
+            queue1.Enqueue(1);
+            queue1.Enqueue(2);
+            queue1.Enqueue(3);
+            queue1.Enqueue(4);
+            queue1.Enqueue(5);
+            Console.WriteLine(queue1.Count);
+            Console.WriteLine(queue1.Dequeue());
+            queue1.Enqueue(6);
+            queue1.Enqueue(7);
+            Console.WriteLine(queue1.Count);
+            Console.WriteLine(queue1.Peek());
+            Console.WriteLine(queue1.Count);
+        }
+    }
+}
---
+++ 2019/Grebenchuk/QueueClass/QueueClass/Queue.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueClass
+{
+    class UsualQueue <T>
+    {
+        const int N = 5;
+        T[] queue;
+        int begin;
+        int end;
+        int count;
+
+        public UsualQueue()
+        {
+            queue = new T[N];
+            begin = 0;
+            end = 0;
+        }
+
+        public UsualQueue(int k)
+        {
+            queue = new T[k];
+            begin = 0;
+            end = 0;
+        }
+
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+        }
+
+        int Size
+        {
+            get
+            {
+                return queue.Length;
+            }
+        }
+
+        public bool IsEmpty()
+        {
+            if (this.Count == 0) return true;
+            else return false;
+        }
+
+        public void Enqueue(T elem)
+        {
+            queue[end] = elem;
+            int tail = (end + 1) % this.Size;
+            if (tail == begin) Increase();
+            else end = tail;
+            count++;
+        }
+
+        public T Dequeue()
+        {
+            if (IsEmpty()) throw new ArgumentOutOfRangeException("The queue is empty");
+            else
+            {
+                int m = begin;
+                begin = (begin + 1) % Size;
+                count--;
+                return queue[m];
+            }
+        }
+
+        public T Peek()
+        {
+            if (IsEmpty()) throw new ArgumentOutOfRangeException("The queue is empty");
+            else return queue[begin];
+        }
+
+        public void Clear()
+        {
+            Array.Clear(queue, 0, Size);
+            queue = new T[N];
+            begin = 0;
+            end = 0;
+            count = 0;
+        }
+
+        void Increase()
+        {
+            T[] mas = new T[Size * 2];
+            int m = begin;
+            int i = 0;
+            while(m!=end)
+            {
+                mas[i] = queue[m];
+                m = (m + 1) % Size;
+                i++;
+            }
+            begin = 0;
+            end = Count;
+            queue = mas;
+        }
+    }
+}

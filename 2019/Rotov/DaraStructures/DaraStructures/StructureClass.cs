2f7834fd762ce0539a289e45fb76661d82ba04af
Vladislav <trol7540@gmail.com>
2019-07-11 00:01:18 +0300

    binary search tree

---
+++ 2019/Rotov/DaraStructures/DaraStructures/StructureClass.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace DaraStructures
+{
+    public class Node<T>
+    {
+        public int Key { get; set; }
+        public T Value { get; set; }
+
+        public Node<T> Left { get; set; } 
+
+        public Node<T> Right { get; set; }
+
+        public Node(int key, T value)
+        {
+            Key = key;
+            Value = value;
+        }
+    }
+    public class BinarySearchTree<T>
+    {
+        public Node<T> Root { get; set; }
+        public int Count { get; private set; }
+
+        public BinarySearchTree() { }
+        public BinarySearchTree(Node<T> node)
+        {
+            Insert(node);
+        }
+
+
+        private Node<T> RecursiveDiving(Node<T> node, int key)
+        {
+            if (node.Key == key) return node;
+            else if (key < node.Key)
+            {
+                if (node.Left is null) return node;
+                else return RecursiveDiving(node.Left, key);
+            }
+            else
+            {
+                if (node.Right is null) return node;
+                else return RecursiveDiving(node.Right, key);
+            }
+        }
+
+        public void Insert(Node<T> node)
+        {
+            if (Root is null) { Root = node; Count++; }
+            else
+            {
+                Node<T> current = RecursiveDiving(Root, node.Key);
+                if (current.Key == node.Key) current.Value = node.Value;
+                else if (node.Key < current.Key) { current.Left = node; Count++; }
+                else { current.Right = node; Count++; }
+            }
+        }
+
+        public Node<T> Find(int key)
+        {
+            if (Count == 0) return null;
+            Node<T> current = RecursiveDiving(Root, key);
+            if (current.Key == key) return current;
+            else return null;
+        }
+
+        public void Remove(int key)
+        {
+            var result = RecursiveRemove(Root, key);
+        }
+
+        public Node<T> RecursiveRemove(Node<T> current, int key)
+        {
+            if (current is null) return current;
+            if (key < current.Key)
+            {
+                current.Left = RecursiveRemove(current.Left, key);
+            } else if (key > current.Key)
+            {
+                current.Right = RecursiveRemove(current.Right, key);
+            } else if (current.Left != null && current.Right != null)
+            {
+                Node<T> find = FindMinimumNode(current.Right);
+                current.Key = find.Key;
+                current.Value = find.Value;
+                current.Right = RecursiveRemove(current.Right, current.Key);
+            }
+            else
+            {
+                if (current.Left != null) current = current.Left;
+                else current = current.Right;
+                Count--;
+            }
+            return current;
+        }
+
+        public Node<T> FindMinimumNode(Node<T> current)
+        {
+            while (current.Left != null) current = current.Left;
+            return current;
+        }
+    }
+}
---
+++ 2019/Rotov/DaraStructures/teststructure/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DaraStructures;
+
+namespace teststructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinarySearchTree<int> tree = new BinarySearchTree<int>();
+            tree.Insert(new Node<int>(8, 1));
+            tree.Insert(new Node<int>(3, 1));
+            tree.Insert(new Node<int>(10, 1));
+            tree.Insert(new Node<int>(1, 1));
+            tree.Insert(new Node<int>(6, 1));
+            tree.Insert(new Node<int>(14, 1));
+            tree.Insert(new Node<int>(4, 1));
+            tree.Insert(new Node<int>(7, 1));
+            tree.Insert(new Node<int>(13, 1));
+            tree.Remove(3);
+        }
+    }
+}

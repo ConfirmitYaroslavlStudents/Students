f11499048cca7cb69b07c936b2a2bbef18e9bb21
medegor44 <medegor44@gmail.com>
2019-07-11 16:25:48 +0300

    Add LinkedList project

---
+++ 2019/Medvedev/LinkedList/LinkedList/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> list = new LinkedList<int>(new int[] { 1, 2, 3, 4 });
+
+            foreach (var x in list)
+                Console.WriteLine(x);
+        }
+    }
+}

d8f8b450e86ac6f29737ca8b549a95600fd04238
LenaVi <lenavi@user.com>
2019-07-18 18:01:55 +0300

    Add Tests

---
+++ 2019/Vasileva/DoubleLinkedList/DllLIb/Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DllLIb
+{
+    internal class Node<T>
+    {
+        public T Value { get; set; }
+        public Node<T> Previous { get; set; }
+        public Node<T> Next { get; set; }
+        public Node(T value)
+        {
+            Value = value;
+        }
+    }
+}
---
+++ 2019/Vasileva/DoubleLinkedList/DllTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DllTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DllTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("91f95af2-d452-412b-94bb-73a4f4e3e5a7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

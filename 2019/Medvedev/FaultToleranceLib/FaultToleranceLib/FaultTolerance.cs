4c765f0e9517fdb960474e7bf44550b94bc1561b
medegor44 <medegor44@gmail.com>
2019-07-23 20:25:22 +0300

    Naive library implementation + tests

---
+++ 2019/Medvedev/FaultToleranceLib/FaultToleranceLib/FaultTolerance.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace FaultToleranceLib
+{
+    public class FaultTolerance
+    {
+        // TODO: Probably static methods?
+        // TODO: Probably multiple exception support?
+        private void DoAction(Type exceptionType, Action action, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                try
+                {
+                    action();
+                }
+                catch (Exception ex)
+                {
+                    if (ex.GetType() == exceptionType && i < count - 1)
+                        continue;
+                    else
+                        throw ex;
+                }
+                break;
+            }
+        }
+
+        public void Try(Type exceptionType, Action action, int count)
+        {
+            try
+            {
+                DoAction(exceptionType, action, count);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            
+        }
+
+        public void TryFallback(Type exceptionType, Action action, int count, Action fallback)
+        {
+            try
+            {
+                DoAction(exceptionType, action, count);
+            }
+            catch (Exception ex)
+            {
+                if (ex.GetType() == exceptionType)
+                    fallback();
+                else
+                    throw;
+            }
+        }
+    }
+}
---
+++ 2019/Medvedev/FaultToleranceLib/FaultToleranceLibTests/FaultToleranceLibTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Xunit;
+
+namespace FaultToleranceLib.Tests
+{
+    public class FaultToleranceLibTests
+    {
+        [Fact]
+        public void Try_Failed_ThrowException()
+        {
+            Action action = () => throw new NotImplementedException();
+
+            FaultTolerance faultTolerance = new FaultTolerance();
+
+            Assert.Throws<NotImplementedException>(() => faultTolerance.Try(typeof(NotImplementedException), action, 2));
+        }
+
+        [Fact]
+        public void TryFallback_Failed_RunFallback()
+        {
+            bool fallbackHadRun = false;
+
+            Action action = () => throw new NotImplementedException();
+            Action fallback = () => fallbackHadRun = true;
+
+            FaultTolerance faultTolerance = new FaultTolerance();
+
+            faultTolerance.TryFallback(typeof(NotImplementedException), action, 2, fallback);
+
+            Assert.True(fallbackHadRun);
+        }
+
+        [Theory]
+        [InlineData(3)]
+        [InlineData(4)]
+        public void Try_Succeed_NoExceptionThrown(int d)
+        {
+            FaultTolerance faultTolerance = new FaultTolerance();
+
+            int count = 1;
+            Action action = () => {
+                int z = count / d;
+                count++;
+
+                int y = 1 / z;
+            };
+
+            faultTolerance.Try(typeof(DivideByZeroException), action, 3);
+        }
+
+        [Theory]
+        [InlineData(3)]
+        [InlineData(4)]
+        public void TryFallback_Succed_NoFallbackRun(int d)
+        {
+            FaultTolerance faultTolerance = new FaultTolerance();
+            bool fallbackHadRun = false;
+
+            int count = 1;
+            Action action = () => {
+                int z = count / d;
+                count++;
+
+                int y = 1 / z;
+            };
+
+            Action fallback = () => fallbackHadRun = true;
+
+            faultTolerance.TryFallback(typeof(DivideByZeroException), action, 3, fallback);
+
+            Assert.False(fallbackHadRun);
+        }
+    }
+}

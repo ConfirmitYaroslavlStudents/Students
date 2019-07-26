85d91866f72110072f1edbf2dcb1e9107b55f2a5
beSilently <beSilently@users.noreply.github.com>
2019-07-26 14:13:16 +0300

    Update MultipleStrategiesTests.cs

--- 2019/Goncharova/FaultToleranceLib/FaultTolerance/FaultToleranceTests/MultipleStrategiesTests.cs
+++ 2019/Goncharova/FaultToleranceLib/FaultTolerance/FaultToleranceTests/MultipleStrategiesTests.cs
@@ -18,9 +18,7 @@ namespace FaultToleranceTests
 
             var fallbackStrategy = new FallbackStrategy(new InvalidCastException(), fallbackAction);
             fallbackStrategy.Execute(
-                () =>
-                {
-                    retryStrategy.Execute(
+                () => { retryStrategy.Execute(
                 () => helper.ThrowException());
                 });
 
@@ -38,13 +36,66 @@ namespace FaultToleranceTests
 
             var fallbackStrategy = new FallbackStrategy(new InvalidCastException(), fallbackAction);
             fallbackStrategy.Execute(
-                () =>
-                {
-                    retryStrategy.Execute(
+                () => { retryStrategy.Execute(
                 () => helper.ThrowException());
                 });
 
             Assert.False(fallbackActionIsRun);
         }
+
+        [Fact]
+        public void FallbackAction_RetryDoesNotHandleExceptionButFallbackDoes_ShouldBeRun()
+        {
+            var retryStrategy = new RetryStrategy(new InvalidCastException(), 3);
+            
+            bool fallbackActionIsRun = false;
+            void fallbackAction() { fallbackActionIsRun = true; }
+
+            var fallbackStrategy = new FallbackStrategy(new DivideByZeroException(), fallbackAction);
+            fallbackStrategy.Execute(
+                () => {
+                    retryStrategy.Execute(
+            () => { throw new DivideByZeroException(); });
+                });
+
+            Assert.True(fallbackActionIsRun);
+        }
+
+        [Fact]
+        public void RetryAndFallback_BothDoNotHandleException_ShouldThrow()
+        {
+            var retryStrategy = new RetryStrategy(new InvalidCastException(), 3);
+
+            void fallbackAction() { }
+
+            var fallbackStrategy = new FallbackStrategy(new DivideByZeroException(), fallbackAction);
+            void fallbackAfterRetry() => fallbackStrategy.Execute(
+                () =>
+                {
+                    retryStrategy.Execute(
+            () => { throw new IndexOutOfRangeException(); });
+                });
+
+            Assert.Throws<IndexOutOfRangeException>(fallbackAfterRetry);
+        }
+
+        [Fact]
+        public void FallbackAction_FallbackHandlesException_ShouldBeRun()
+        {
+            var retryStrategy = new RetryStrategy(new InvalidCastException(), 3);
+
+            bool fallbackActionIsRun = false;
+            void fallbackAction() { fallbackActionIsRun = true; }
+
+            var fallbackStrategy = new FallbackStrategy(new DivideByZeroException(), fallbackAction);
+            retryStrategy.Execute(
+                () => {
+                    fallbackStrategy.Execute(
+            () => { throw new DivideByZeroException(); });
+                });
+
+            Assert.True(fallbackActionIsRun);
+        }
+
     }
 }

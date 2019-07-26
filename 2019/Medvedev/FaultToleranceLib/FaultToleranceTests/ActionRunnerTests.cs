6bcc6aa7c00b3a31da79fccf8b1215a211e59d3f
medegor44 <medegor44@gmail.com>
2019-07-26 20:56:28 +0300

    Fixed another bug in tests

--- 2019/Medvedev/FaultToleranceLib/FaultToleranceTests/ActionRunnerTests.cs
+++ 2019/Medvedev/FaultToleranceLib/FaultToleranceTests/ActionRunnerTests.cs
@@ -50,7 +50,31 @@ namespace FaultTolerance.Tests
                     throw new ArithmeticException();
             };
 
+            var runner = new ActionRunner(param);
+            runner.Try(action);
+
             Assert.True(actual <= upperBound);
+        }
+
+        [Fact]
+        public void Try_CountTimes_Failed_ActionRuns_CountTimes()
+        {
+            int upperBound = 10;
+            int expected = upperBound;
+            var param = new ActionRunnerParameters(upperBound);
+            param.Handle<ArithmeticException>();
+
+            int actual = 0;
+            Action action = () =>
+            {
+                actual++;
+                throw new ArithmeticException();
+            };
+
+            var runner = new ActionRunner(param);
+            runner.Try(action);
+
+            Assert.Equal(expected, actual);
         }
 
         [Fact]

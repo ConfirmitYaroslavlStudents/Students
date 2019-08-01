f945567947f6d783513d7fac26b0aba6e92d7e0f
LenaVi <lenavi@user.com>
2019-08-02 01:14:11 +0300

    primitive realisation SyncApp

---
+++ 2019/Vasilyeva/Synchronization/SyncApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using SyncLib;
+
+namespace SyncApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Synchronization synchronization = new Synchronization(@"C:\Users\stalk\Desktop\vs\Synchronization\SyncLib\bin\Debug\master", @"C:\Users\stalk\Desktop\vs\Synchronization\SyncLib\bin\Debug\slave");
+            synchronization.Synchronaze();
+        }
+    }
+}

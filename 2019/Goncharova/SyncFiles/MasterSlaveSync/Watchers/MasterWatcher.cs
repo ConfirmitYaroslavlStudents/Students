8983c61e6490ae817c7022036288570dd05f6290
beSilently <beSilently@users.noreply.github.com>
2019-08-01 23:12:07 +0300

    Upload SyncFiles project

---
+++ 2019/Goncharova/SyncFiles/MasterSlaveSync/Watchers/MasterWatcher.cs
@@ -0,0 +1,80 @@
+﻿using System.IO;
+
+namespace MasterSlaveSync
+{
+    public class MasterWatcher
+    {
+        private DirectoryInfo slave;
+        private DirectoryInfo master;
+        public MasterWatcher(DirectoryInfo master, DirectoryInfo slave)
+        {
+            this.slave = slave;
+            this.master = master;
+        }
+
+        public void WatchDirectory()
+        {
+            using (FileSystemWatcher watcher = new FileSystemWatcher())
+            {
+                watcher.Path = master.FullName;
+
+                watcher.NotifyFilter = NotifyFilters.LastWrite
+                                     | NotifyFilters.FileName;
+
+                watcher.Changed += OnChanged;
+                watcher.Created += OnCreated;
+                watcher.Deleted += OnDeleted;
+                watcher.Renamed += OnRenamed;
+
+                watcher.EnableRaisingEvents = true;
+                while (true) ;
+            }
+
+        }
+
+        private void OnChanged(object source, FileSystemEventArgs e)
+        {
+            SyncEngine.ResolveConflict(
+                new Conflict(new FileInfo(e.FullPath),
+                WatcherHelpers.GetFileWithTheSameName(slave.FullName, e.Name)));
+        }
+
+        private void OnCreated(object source, FileSystemEventArgs e)
+        {
+            if (!File.Exists(Path.Combine(slave.FullName, e.Name)))
+            {
+                WatcherHelpers.CreateFile(slave.FullName, e.Name);
+            }
+            else
+            {
+                SyncEngine.ResolveConflict(
+                     new Conflict(new FileInfo(e.FullPath),
+                     WatcherHelpers.GetFileWithTheSameName(slave.FullName, e.Name)));
+            }
+
+        }
+
+        private void OnDeleted(object source, FileSystemEventArgs e)
+        {
+            WatcherHelpers.DeleteFile(slave.FullName, e.Name);
+        }
+
+        private void OnRenamed(object source, RenamedEventArgs e)
+        {
+
+            if (!File.Exists(Path.Combine(slave.FullName, e.Name)))
+            {
+                WatcherHelpers.CreateFile(slave.FullName, e.Name);
+            }
+            else
+            {
+                SyncEngine.ResolveConflict(
+                     new Conflict(new FileInfo(e.FullPath),
+                     WatcherHelpers.GetFileWithTheSameName(slave.FullName, e.Name)));
+            }
+
+            WatcherHelpers.DeleteFile(slave.FullName, e.OldName);
+
+        }
+    }
+}
---
+++ 2019/Goncharova/SyncFiles/MasterSlaveSync/Watchers/WatcherHelpers.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+
+namespace MasterSlaveSync
+{
+    internal static class WatcherHelpers
+    {
+        internal static FileInfo GetFileWithTheSameName(string directoryPath, string fileName)
+        {
+            var directory = new DirectoryInfo(directoryPath);
+            var result = directory.GetFiles(fileName);
+
+            return result[0];
+        }
+
+        internal static void CreateFile(string directoryPath, string fileName)
+        {
+            File.Create(Path.Combine(directoryPath, fileName));
+        }
+
+        internal static void DeleteFile(string directoryPath, string fileName)
+        {
+            File.Delete(Path.Combine(directoryPath, fileName));
+        }
+        internal static void CopyFile(string srcDirectoryPath, string destDirectoryPath, string fileName)
+        {
+            var srcFile = GetFileWithTheSameName(srcDirectoryPath, fileName);
+            File.Copy(srcFile.FullName, Path.Combine(destDirectoryPath, fileName));
+        }
+    }
+}

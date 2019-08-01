a059ac7dee3cdbc82655b0ed47f81e55a9d42627
Vladislav <vladislav.rotov@ori.team>
2019-08-01 09:27:57 +0300

    add recursive version

---
+++ 2019/Rotov/SynchLibrary/SynchLibrary/FileHandler.cs
@@ -0,0 +1,52 @@
+﻿using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SynchLibrary
+{
+    public class FileHandler
+    {
+        string MasterPath { get; set; }
+        string SlavePath { get; set; }
+
+        public FileHandler(string master, string slave)
+        {
+            MasterPath = master;
+            SlavePath = slave;
+        }
+
+        public void MigrateForFiles()
+        {
+            var listMaster = new List<WrapperForFile>();
+            foreach (var file in Directory.GetFiles(MasterPath, "*", SearchOption.AllDirectories))
+                listMaster.Add(new WrapperForFile(new FileInfo(file), MasterPath));
+            var listSlave = new List<WrapperForFile>();
+            foreach (var file in Directory.GetFiles(SlavePath, "*", SearchOption.AllDirectories))
+                listSlave.Add(new WrapperForFile(new FileInfo(file), SlavePath));
+
+            var intersection = listMaster.Intersect(listSlave, new FileWrapperComparer());
+            var masterWithoutSlave = listMaster.Except(listSlave, new FileWrapperComparer());
+            var slaveWithoutMaster = listSlave.Except(listMaster, new FileWrapperComparer());
+            MoveIntersectionFiles(intersection);
+            MoveSwapFiles(masterWithoutSlave, slaveWithoutMaster);
+        }
+
+        private void MoveIntersectionFiles(IEnumerable<WrapperForFile> files)
+        {
+            foreach (var file in files)
+            {
+                var old = Path.Combine(MasterPath, string.Join(@"\", file.RelativePath));
+                var fresh = Path.Combine(SlavePath, string.Join(@"\", file.RelativePath));
+                File.Copy(old, fresh, true);
+            }
+        }
+
+        private void MoveSwapFiles(IEnumerable<WrapperForFile> master, IEnumerable<WrapperForFile> slave)
+        {
+            foreach (var file in master)
+                File.Copy(Path.Combine(MasterPath, file.RelativePath), Path.Combine(SlavePath, file.RelativePath));
+            foreach (var file in slave)
+                File.Copy(Path.Combine(SlavePath, file.RelativePath), Path.Combine(MasterPath, file.RelativePath));
+        }
+    }
+}
---
+++ 2019/Rotov/SynchLibrary/SynchLibrary/WrapperForFile.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SynchLibrary
+{
+    public class WrapperForFile
+    {
+        public string RelativePath;
+
+        public WrapperForFile(FileInfo file, string root)
+        {
+            SetAbsolutePath(file, root);
+        }
+
+        public void SetAbsolutePath(FileInfo file, string root)
+        {
+            var count = root.Split(new string[] { @"\" }, StringSplitOptions.RemoveEmptyEntries).Length;
+            var parts = file.FullName.Split(new string[] { @"\" }, StringSplitOptions.RemoveEmptyEntries);
+            var relative = new string[parts.Length - count];
+            for (int i = count; i < parts.Length; i++)
+                relative[i - count] = parts[i];
+            RelativePath =  string.Join(@"\", relative);
+        }
+    }
+
+
+    public class FileWrapperComparer: IEqualityComparer<WrapperForFile>
+    {
+        public bool Equals(WrapperForFile x, WrapperForFile y)
+        {
+            return x.RelativePath.SequenceEqual(y.RelativePath);
+        }
+
+        public int GetHashCode(WrapperForFile obj)
+        {
+            return obj.RelativePath.GetHashCode();
+        }
+    }
+
+}

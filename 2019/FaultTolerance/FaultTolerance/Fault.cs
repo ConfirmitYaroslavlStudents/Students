5bdfb9376d644879fdf924c628a10839c1bf2a29
Kirill Vinogradov <kirill.vinogradov@confirmit.com>
2019-08-06 17:48:59 +0300

    Add FaultTolerance;

---
+++ 2019/FaultTolerance/FaultTolerance/Fault.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FaultTolerance
+{
+    public class Fault
+    {
+        private Dictionary<Type, int> _knownExceptions = new Dictionary<Type, int>();
+
+        public Fault Retry<T>(int count)
+        {
+            var type = typeof(T);
+
+            if (_knownExceptions.ContainsKey(type))
+            {
+                _knownExceptions[type] = count;
+            }
+            else
+            {
+                _knownExceptions.Add(type, count);
+            }
+
+            return this;
+        }
+
+        public void Launch(Action action)
+        {
+            while (!TryAction(action))
+            {
+
+            }
+        }
+
+        private bool TryAction(Action action)
+        {
+            try
+            {
+                action();
+            }
+            catch (Exception e)
+            {
+                var type = e.GetType();
+
+                if (!_knownExceptions.ContainsKey(type) || _knownExceptions[e.GetType()] == 0)
+                {
+                    throw;
+                }
+
+                _knownExceptions[type]--;
+
+                return false;
+            }
+
+            return true;
+        }
+    }
+}

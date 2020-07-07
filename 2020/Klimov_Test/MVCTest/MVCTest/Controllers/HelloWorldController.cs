c23c9200c3caf337d235e467cde2a576f1563a35
Egor Klimov <egardoz01@gmail.com>
2020-07-07 21:47:01 +0300

    test web app

---
+++ 2020/Klimov_Test/MVCTest/MVCTest/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVCTest.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Welcome(string name, int cnt)
+        {
+            ViewData["name"] = name;
+            ViewData["cnt"] = cnt;
+            return View();
+        }
+    }
+}

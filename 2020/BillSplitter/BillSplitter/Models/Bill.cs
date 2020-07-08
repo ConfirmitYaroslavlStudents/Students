86ad5953b6ba6654ff6ac8edee46665df632d9ae
medegor44 <medegor44@gmail.com>
2020-07-08 19:40:24 +0300

    Added models

---
+++ 2020/BillSplitter/BillSplitter/Models/Bill.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BillSplitter.Models
+{
+    public class Bill
+    {
+        public int Id { get; set; }
+        public List<Position> Positions { get; }
+        public string Url { get; }
+        // to-add State
+    }
+}
---
+++ 2020/BillSplitter/BillSplitter/Models/Position.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BillSplitter.Models
+{
+    public class Position
+    {
+        public int Id { get; set; }
+        public decimal Price { get; set; }
+        public string Name { get; set; }
+        public List<Customer> Customers { get; }
+
+        public void AddCustomer(Customer customer)
+        {
+            Customers.Add(customer);
+        }
+    }
+}

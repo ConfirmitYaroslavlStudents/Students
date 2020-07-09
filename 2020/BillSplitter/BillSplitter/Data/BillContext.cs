e739d19ae05afadc245432343ce1fc68e7db7d46
medegor44 <medegor44@gmail.com>
2020-07-09 16:46:10 +0300

    Added EF

---
+++ 2020/BillSplitter/BillSplitter/Data/BillContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BillSplitter.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BillSplitter.Data
+{
+    public class BillContext : DbContext
+    {
+        public BillContext(DbContextOptions<BillContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Bill> Bill { get; set; }
+    }
+}
--- 2020/BillSplitter/BillSplitter/Startup.cs
+++ 2020/BillSplitter/BillSplitter/Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BillSplitter.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +26,8 @@ namespace BillSplitter
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<BillContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("BillContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

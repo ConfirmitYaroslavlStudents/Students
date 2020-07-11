a9126fb1997298a884ccb31f213994450c403064
medegor44 <medegor44@gmail.com>
2020-07-11 12:22:03 +0300

    Full webapp from tutorial

--- 2020/WebApplication1/WebApplication1/Models/Movie.cs
+++ 2020/WebApplication1/WebApplication1/Models/Movie.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,9 +13,12 @@ namespace WebApplication1.Models
         public int Id { get; set; }
         public string Title { get; set; }
         
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
+        public string Rating { get; set; }
     }
 }
--- 2020/WebApplication1/WebApplication1/Startup.cs
+++ 2020/WebApplication1/WebApplication1/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
 
 namespace WebApplication1
 {
@@ -24,6 +26,9 @@ namespace WebApplication1
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<MvcMovieContext>(opt =>
+                opt.UseSqlServer(Configuration.GetConnectionString("MvcMovieContext"))
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

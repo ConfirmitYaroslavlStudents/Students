148c0503062c2d750497db148580e51e5fd059ba
medegor44 <medegor44@gmail.com>
2020-07-14 17:41:36 +0300

    Update default route

--- 2020/BillSplitter/BillSplitter/Startup.cs
+++ 2020/BillSplitter/BillSplitter/Startup.cs
@@ -54,7 +54,7 @@ namespace BillSplitter
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=NewBill}/{id?}");
             });
         }
     }

1db74eb971596093ff408f7693161976f3fe4dc0
Смирнов Павел <cocacola76rus@outlook.com>
2020-07-16 13:09:00 +0300

    Smirnov. SkillTree. Built architecture and created minimal functionality.

---
+++ 2020/Smirnov/SkillTree/SkillTree/Classes/User.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SkillTree.Classes
+{
+    [Serializable]
+    public class User: Person
+    {
+        private List<Skill> _availableSkills;
+
+        public User() { }
+        public User(string name) : base (name)
+        {
+            _availableSkills = new List<Skill>();
+        }
+        public bool LearnNewSkill(Skill skill)
+        {
+            if (!_availableSkills.Contains(skill))
+            {
+                _availableSkills.Add(skill);
+                return true;
+            }
+            return false;
+        }
+        public string ReturnAllInformationAboutSkills()
+        {
+            string toString = "";
+            foreach (var c in _availableSkills)
+            {
+                toString += c.ToString();
+            }
+            return toString;
+        }
+        public string ReturnNameAllSkills()
+        {
+            string toString = "";
+            foreach (var c in _availableSkills)
+            {
+                if (toString == "")
+                {
+                    toString = c.Name;
+                }
+                else
+                {
+                    toString = toString + " " + c.Name;
+                }
+            }
+            return toString;
+        }
+
+    }
+}
---
+++ 2020/Smirnov/SkillTree/SkillTree/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Xml.Serialization;
+using System.IO;
+using System.Collections.Generic;
+
+namespace SkillTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Classes.Discipline> Discipline = new List<Classes.Discipline>();
+            do
+            {
+                Console.WriteLine("1 Create new discipline");
+                Console.WriteLine("2 Return all scills for discipline");
+                Console.WriteLine("3 Save and Exit");
+                switch (Console.ReadLine())
+                {
+                    case ("1"):
+                        Console.Clear();
+                        Classes.Admin admin = new Classes.Admin("admin");
+
+                        admin.CreateNewDiscipline("Programming", ref Discipline);
+
+                        List<Classes.Skill> skills = new List<Classes.Skill>();
+                        skills.Add(new Classes.Skill("Encapsulation", "easy", "important", new List<Classes.Skill>(), 10));
+                        skills.Add(new Classes.Skill("Polymorphism", "easy", "important", new List<Classes.Skill>(), 15));
+                        skills.Add(new Classes.Skill("Inheritance", "easy", "important", new List<Classes.Skill>(), 20));
+
+                        admin.CreateNewDiscipline("OOP", skills, ref Discipline);
+                                               
+                        break;
+                    case ("2"):
+                        Console.Clear();
+                        Console.WriteLine("Write name discipline ");
+                        string Name = Console.ReadLine();
+                        foreach(var c in Discipline)
+                        {
+                            if (c.Name == Name)
+                                Console.WriteLine(c.ReturnNameAllSkills());
+                        }
+                        break;
+                    case ("3"):
+                        foreach (var c in Discipline)
+                        {
+                            XmlSerializer formatter = new XmlSerializer(typeof(Classes.Discipline));
+                            using (FileStream fs = new FileStream(@"Discipline\" + c.Name + ".xml", FileMode.OpenOrCreate))
+                            {
+                                formatter.Serialize(fs, c);
+                            }
+                        }
+                        return;                       
+                }                
+            }
+            while (true);
+
+        }
+    }
+}

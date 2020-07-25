69397cd44d005a299b6a9ed17a429731a1c8823e
medegor44 <medegor44@gmail.com>
2020-07-25 13:30:37 +0300

    Stage interaction level models

---
+++ 2020/BillSplitter/BillSplitter/Models/InteractionLevel/InteractionLevelPosition.cs
@@ -0,0 +1,12 @@
+﻿namespace BillSplitter.Models.InteractionLevel
+{
+    public class InteractionLevelPosition
+    {
+        public int Id { get; set; }
+        public decimal Price { get; set; }
+        public string Name { get; set; }
+        public int QuantityNumerator { get; set; } = 0;
+        public int QuantityDenomenator { get; set; } = 1;
+        public bool Selected { get; set; } = false;
+    }
+}

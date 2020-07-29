bb043bfa4e574931b6e1ddb7a148dca88b3b4566
medegor44 <medegor44@gmail.com>
2020-07-29 13:47:28 +0300

    Add validators

---
+++ 2020/BillSplitter/BillSplitterTests/ValidatorTests.cs
@@ -0,0 +1,50 @@
+﻿using BillSplitter.Validators;
+using Xunit;
+
+namespace BillSplitterTests
+{
+    public class ValidatorTests
+    {
+        [Fact]
+        public void ValidateInconsistentData_ValidationFailed()
+        {
+            var validator = new Validator<int>();
+            validator.AddValidation(x => x % 2 == 0);
+
+            Assert.False(validator.Validate(3));
+        }
+
+        [Fact]
+        public void ValidateConsistentData_ValidationComplete()
+        {
+            var validator = new Validator<int>();
+            validator.AddValidation(x => x % 2 == 0);
+
+            Assert.True(validator.Validate(2));
+        }
+
+        [Fact]
+        public void ChainValidation_ConsistentData_ValidationComplete()
+        {
+            var validator = new Validator<int>();
+            validator
+                .AddValidation(x => x % 3 == 0)
+                .AddValidation(x => x % 2 == 0)
+                .AddValidation(x => x < 10);
+
+            Assert.True(validator.Validate(6));
+        }
+
+        [Fact]
+        public void ChainValidation_InconsistentData_ValidationFailed()
+        {
+            var validator = new Validator<int>();
+            validator
+                .AddValidation(x => x % 3 == 0)
+                .AddValidation(x => x % 2 == 0)
+                .AddValidation(x => x < 10);
+
+            Assert.True(validator.Validate(15));
+        }
+    }
+}

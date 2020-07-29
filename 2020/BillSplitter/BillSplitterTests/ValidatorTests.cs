f0cceb4f4be3baca33891be90160f0bb3c19707d
medegor44 <medegor44@gmail.com>
2020-07-29 13:58:45 +0300

    Fix tests

--- 2020/BillSplitter/BillSplitterTests/ValidatorTests.cs
+++ 2020/BillSplitter/BillSplitterTests/ValidatorTests.cs
@@ -8,8 +8,7 @@ namespace BillSplitterTests
         [Fact]
         public void ValidateInconsistentData_ValidationFailed()
         {
-            var validator = new Validator<int>();
-            validator.AddValidation(x => x % 2 == 0);
+            var validator = new Validator<int>().AddValidation(x => x % 2 == 0);
 
             Assert.False(validator.Validate(3));
         }
@@ -17,8 +16,7 @@ namespace BillSplitterTests
         [Fact]
         public void ValidateConsistentData_ValidationComplete()
         {
-            var validator = new Validator<int>();
-            validator.AddValidation(x => x % 2 == 0);
+            var validator = new Validator<int>().AddValidation(x => x % 2 == 0);
 
             Assert.True(validator.Validate(2));
         }
@@ -26,8 +24,7 @@ namespace BillSplitterTests
         [Fact]
         public void ChainValidation_ConsistentData_ValidationComplete()
         {
-            var validator = new Validator<int>();
-            validator
+            var validator = new Validator<int>()
                 .AddValidation(x => x % 3 == 0)
                 .AddValidation(x => x % 2 == 0)
                 .AddValidation(x => x < 10);
@@ -38,13 +35,12 @@ namespace BillSplitterTests
         [Fact]
         public void ChainValidation_InconsistentData_ValidationFailed()
         {
-            var validator = new Validator<int>();
-            validator
+            var validator = new Validator<int>()
                 .AddValidation(x => x % 3 == 0)
                 .AddValidation(x => x % 2 == 0)
                 .AddValidation(x => x < 10);
 
-            Assert.True(validator.Validate(15));
+            Assert.False(validator.Validate(15));
         }
     }
 }

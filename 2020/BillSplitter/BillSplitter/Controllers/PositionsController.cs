ce46c56b31393fba9cacb50ba4ce842f2047c429
Egor Klimov <egardoz01@gmail.com>
2020-08-03 15:45:03 +0300

    Positions controller validation

--- 2020/BillSplitter/BillSplitter/Controllers/PositionsController.cs
+++ 2020/BillSplitter/BillSplitter/Controllers/PositionsController.cs
@@ -3,27 +3,56 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using BillSplitter.Models;
+using System.Diagnostics;
 
 namespace BillSplitter.Controllers
 {
     public class PositionsController : Controller // TODO ViewModels
     {
-        private BillsDbAccessor _billDbAccessor;
-        private PositionsDbAccessor _positionsDbAccessor;
-
+        private readonly BillsDbAccessor _billDbAccessor;
+        private readonly PositionsDbAccessor _positionsDbAccessor;
+        private readonly UserIdVisitor _visitor;
         public PositionsController(BillContext context, UserIdVisitor userIdVisitor)
         {
             _billDbAccessor = new BillsDbAccessor(context);
             _positionsDbAccessor = new PositionsDbAccessor(context);
+            _visitor = userIdVisitor;
         }
+
+        private bool ValidateUser(int billId)
+        {
+            var bill = _billDbAccessor.GetBillById(billId);
+            if (bill == null) 
+                return false;
+            if (bill.UserId != _visitor.GetUserId(this))
+                return false;
+
+           return true;
+        }
+
+        private bool ValidatePosition(int billId, int positionId)
+        {
+            var bill = _billDbAccessor.GetBillById(billId);
+
+            if (bill.Positions.Find(x => x.Id == positionId) == null)
+                return false;
+
+            return true;
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
 
         [Authorize]
         [HttpGet]
         [Route("Bills/{billId}/Positions")]
         public IActionResult Index(int billId)
         {
-            if (!_billDbAccessor.DbContains(billId))
-                throw new NotImplementedException("Case is not implemented yet");
+            if (!ValidateUser(billId))
+                return Error();
 
             ViewData["billId"] = billId;
             var positions = _billDbAccessor.GetBillById(billId).Positions;
@@ -36,8 +65,8 @@ namespace BillSplitter.Controllers
         [Route("Bills/{billId}/Positions")]
         public IActionResult Create(int billId, Position position)
         {
-            if (!_billDbAccessor.DbContains(billId))
-                throw new NotImplementedException("Case is not implemented yet");
+            if (!ValidateUser(billId))
+                return Error();
 
             _positionsDbAccessor.AddPosition(position);
 
@@ -49,8 +78,8 @@ namespace BillSplitter.Controllers
         [Route("Bills/{billId}/Positions/{positionId}")]
         public IActionResult Delete(int billId, int positionId)
         {
-            if (!_billDbAccessor.DbContains(billId))
-                throw new NotImplementedException("Case is not implemented yet");
+            if (!ValidateUser(billId) || !ValidatePosition(billId, positionId))
+                return Error();
 
             _positionsDbAccessor.DeleteById(positionId);
 
@@ -62,8 +91,8 @@ namespace BillSplitter.Controllers
         [Route("Bills/{billId}/Positions/{positionId}/Update")] // How to implement RESTful?
         public IActionResult Update(int billId, int positionId, Position position)
         {
-            if (!_billDbAccessor.DbContains(billId))
-                throw new NotImplementedException("Case is not implemented yet");
+            if (!ValidateUser(billId) || !ValidatePosition(billId, positionId))
+                return Error();
 
             _positionsDbAccessor.UpdateById(positionId, position);
             return RedirectToAction(nameof(Index), new { billId });

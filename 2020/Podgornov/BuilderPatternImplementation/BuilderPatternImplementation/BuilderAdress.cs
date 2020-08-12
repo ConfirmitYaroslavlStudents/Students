a6bfe438b25a60c4830dfa09f506326723a38cdd
Илья Подгорнов <lokkibro@outlook.com>
2020-08-13 00:03:22 +0300

    update skilltree, implement BuilderAddress

---
+++ 2020/Podgornov/BuilderPatternImplementation/BuilderPatternImplementation/BuilderAdress.cs
@@ -0,0 +1,64 @@
+﻿
+
+namespace BuilderPatternImplementation
+{
+    class AddressBuilder
+    {
+        private readonly Address _address;
+
+        public AddressBuilder()
+        {
+            _address = new Address();
+        }
+
+        private AddressBuilder(AddressBuilder addressBuilder)
+        {
+            _address = new Address()
+            {
+                City = addressBuilder._address.City,
+                Street = addressBuilder._address.Street,
+                House = addressBuilder._address.House,
+                Building = addressBuilder._address.Building,
+                Flat = addressBuilder._address.Flat
+            };
+        }
+
+        public AddressBuilder AddCity(string city)
+        {
+            var newBuilderAddress =  new AddressBuilder(this);
+            newBuilderAddress._address.City = city;
+            return newBuilderAddress;
+        }
+
+        public AddressBuilder AddStreet(string street)
+        {
+            var newBuilderAddress = new AddressBuilder(this);
+            newBuilderAddress._address.Street = street;
+            return newBuilderAddress;
+        }
+
+        public AddressBuilder AddHouse(int house)
+        {
+            var newBuilderAddress = new AddressBuilder(this);
+            newBuilderAddress._address.House = house;
+            return newBuilderAddress;
+        }
+
+        public AddressBuilder AddBuilding(int builder)
+        {
+            var newBuilderAddress = new AddressBuilder(this);
+            newBuilderAddress._address.Building = builder;
+            return newBuilderAddress;
+        }
+
+        public AddressBuilder AddFlat(int flat)
+        {
+            var newBuilderAddress = new AddressBuilder(this);
+            newBuilderAddress._address.Flat = flat;
+            return newBuilderAddress;
+        }
+
+        public Address Build() => _address;
+
+    }
+}
---
+++ 2020/Podgornov/BuilderPatternImplementation/BuilderPatternImplementation/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderPatternImplementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var addressBuilder = new AddressBuilder().AddCity("Moscow");
+            var addr1 = addressBuilder.AddBuilding(1).AddFlat(1).Build();
+            var addr2 = addressBuilder.AddFlat(2).Build();
+            Console.WriteLine(addr1 != addr2);
+            Console.ReadKey();
+        }
+    }
+}

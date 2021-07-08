0a57c899d4dcd3c44888652ffb91bdc56f26038e
annanikeshina2018 <annanikeshina2018@gmail.com>
2021-07-08 22:07:06 +0300

    Implementation of stack

---
+++ 2021/Nikeshina/StackStructure/StackStructure/Stack.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StackStructure
+{
+    public class Stack<T>
+    {
+        public int Count { get; private set; }
+        private List<T> elements;
+        public  Stack()
+        {
+            elements = new List<T>();
+            Count = 0;
+        }
+        public void Push(T current)
+        {
+            if (elements.Count < Count)
+                elements[Count-1] = current;
+            else
+                elements.Add(current);
+            Count=Count+1;
+        }
+        public T Pop()
+        {
+            if (Count > 0)
+            {
+                Count--;
+                return elements[Count];
+            }
+            else
+                throw new NullReferenceException();
+            
+        }
+        public T Peek()
+        {
+            if (Count > 0)
+                return elements[Count - 1];
+            else
+                throw new NullReferenceException();
+        }
+    }
+}

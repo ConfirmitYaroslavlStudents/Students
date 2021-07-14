c9b0dd650cdfca6ced010f9cd1ae986ed001ad6e
a.baulina@stud.uniyar.ac.ru <a.baulina@stud.uniyar.ac.ru>
2021-07-15 00:40:04 +0300

    To-Do List console application

---
+++ 2021/Baulina/MyToDoList/MyToDoList/FirstExecutionDeterminer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace ToDoListConsole
+{
+    static class FirstExecutionDeterminer
+    {
+        public static bool FirstRun { get; private set; }
+
+        public static void DetermineWhetherItIsAFirstRun()
+        {
+            var path = Path.Combine(Environment.GetFolderPath(
+                Environment.SpecialFolder.LocalApplicationData), "FirstRun.txt");
+            if (File.Exists(path))
+            {
+                FirstRun = false;
+            }
+            else
+            {
+                CreateEmptyFile(path);
+            }
+        }
+
+        static void CreateEmptyFile(string fileName)
+        {
+            File.Create(fileName).Dispose();
+            File.SetAttributes(fileName, File.GetAttributes(fileName) | FileAttributes.Hidden);
+        }
+    }
+}
---
+++ 2021/Baulina/MyToDoList/MyToDoList/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Spectre.Console;
+
+namespace ToDoListConsole
+{
+    class Program
+    {
+        static void Main()
+        {
+            var uploadProcessor = new StartProcessor();
+            uploadProcessor.Process();
+            FirstExecutionDeterminer.DetermineWhetherItIsAFirstRun();
+            var menuManager = new MenuManager(uploadProcessor.MyToDoList, new MessagePrinter());
+            AppDomain.CurrentDomain.ProcessExit += Exit;
+
+            while (true)
+            {
+                PrintMenu(menuManager);
+            }
+            
+            void Exit(object sender, EventArgs e)
+            {
+                DataSerializer.Serialize(menuManager.MyToDoList);
+            }
+        }
+        
+        public static void PrintMenu(MenuManager manager)
+        {
+            var operation = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                    .Title("[bold plum3] What do you want to do? [/]")
+                    .PageSize(10)
+                    .MoreChoicesText("[grey](Move up and down to reveal more operations)[/]")
+                    .AddChoices("Add", "Edit", "Mark as complete", "Delete", "View all tasks", "Exit"));
+            OperationProcessor(operation, manager);
+        }
+
+        public static void OperationProcessor(string operation, MenuManager manager)
+        {
+            switch (operation)
+            {
+                case "Add":
+                {
+                    manager.Add();
+                    break;
+                }
+                case "Edit":
+                {
+                    manager.Edit();
+                    break;
+                }
+                case "Mark as complete":
+                {
+                    manager.MarkAsComplete();
+                    break;
+                }
+                case "Delete":
+                {
+                    manager.Delete();
+                        break;
+                }
+                case "View all tasks":
+                {
+                    manager.ViewAllTasks();
+                    break;
+                }
+                case "Exit":
+                {
+                    manager.Exit();
+                    break;
+                }
+                default:
+                {
+                    manager.PrintErrorMessage();
+                    break;
+                }
+            }
+        }
+
+    }
+}

41121c09e1f68d7c7cdc94c02df9fb884b2cede5
kachaeva-ysu <anakuro2511@gmail.com>
2021-07-15 15:32:39 +0300

    To do list files

---
+++ 2021/Kachaeva/ToDoList/ToDoList/Task.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ToDoList
+{
+    [Serializable]
+    public class Task
+    {
+        public string text;
+        public bool isDone;
+
+        public Task(string text)
+        {
+            this.text = text;
+        }
+
+        public void ChangeText(string newText)
+        {
+            text = newText;
+        }
+
+        public void ChangeStatus()
+        {
+            isDone = !isDone;
+        }
+    }
+}
---
+++ 2021/Kachaeva/ToDoList/ToDoList/ToDoList.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoList
+{
+    [Serializable]
+    public class ToDoList
+    {
+        private List<Task> tasks = new List<Task>();
+        public int Count { get { return tasks.Count; } }
+
+        public Task this[int index]
+        {
+            get { return tasks[index]; }
+        }
+
+        public void Add(string task)
+        {
+            tasks.Add(new Task(task));
+        }
+
+        public void Remove(int number)
+        {
+            tasks.RemoveAt(number);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder toDoList = new StringBuilder();
+            for (var i = 0; i < tasks.Count; i++)
+            {
+                toDoList.Append(i + 1 + ". " + tasks[i].text);
+                if (tasks[i].isDone)
+                    toDoList.AppendLine("  [v]");
+                else
+                    toDoList.AppendLine("  [ ]");
+            }
+            return toDoList.ToString();
+        }
+    }
+}

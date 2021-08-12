f00e35428537ca7620c3b580aa6aac257cfa7fce
annanikeshina2018 <annanikeshina2018@gmail.com>
2021-08-12 21:11:29 +0300

    upd

---
+++ 2021/Nikeshina/myWeb/myWeb/Controllers/StringHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace myWeb.Controllers
+{
+    [ApiController]
+    [Route("strings")]
+    public class StringHandler : ControllerBase
+    {
+        private int sum;
+        private readonly ILogger<StringHandler> _logger;
+
+        public StringHandler(ILogger<StringHandler> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public int Get()
+        {
+            _logger.LogInformation(sum.ToString());
+            return sum;
+        }
+
+        [HttpPost]
+        public int Post([FromBody] string text)
+        {
+            sum += text.Length;
+            _logger.LogInformation(sum.ToString());
+            return text.Length;
+        }
+    }
+}

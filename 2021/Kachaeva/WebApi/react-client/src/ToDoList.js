61f2f89975bdb00229ec6c23512e2c3e9d6f5deb
Lenovo <Lenovo@DESKTOP-0D9QCLC>
2021-08-25 21:38:10 +0300

    Search added

--- 2021/Kachaeva/WebApi/react-client/src/ToDoList.js
+++ 2021/Kachaeva/WebApi/react-client/src/ToDoList.js
@@ -9,7 +9,10 @@ class ToDoList extends React.Component{
       this.state = {
         newTaskText: "",
         newTaskStatus: false,
-        newTaskTags:"",
+        newTaskTags: [],
+        tagsForSearch: [],
+        searchCondition: "AND",
+        filteredTasks: [],
         toDoList: []
       }
     }
@@ -32,30 +35,129 @@ class ToDoList extends React.Component{
       return (
         <div>
           <h2>To do list</h2>
+          {this.renderSearchTable()}
+          {this.renderFilteredTasksTable()}
           {this.renderAddTaskTable()}    
           {this.renderToDoListTable()}
         </div>
       );
     }
+
+    renderSearchTable = () => {
+      return(
+        <div style={{background:'#e0ffff'}}>
+          <h4>Enter tags: </h4>
+          <table>
+            <tr>
+              <td>
+                <input type="text" onChange={this.changeTagsForSearch} value={this.state.tagsForSearch} />
+              </td>
+              <td>
+                <select onChange={this.changeSearchCondition}>
+                  <option>AND</option>
+                  <option>OR</option>
+                </select>
+              </td>
+              <td>
+                <input type="button" defaultValue="Search" onClick={this.getFilteredTasks}/>
+              </td>
+            </tr>
+          </table>
+        </div>
+      )
+    }
+
+    changeTagsForSearch = e => {
+      this.setState({tagsForSearch: e.target.value.split(/\s*,\s*/)});
+    }
+
+    changeSearchCondition = e => {
+      this.setState({searchCondition: e.target.value});
+    }
+
+    renderFilteredTasksTable = () => {
+      if (this.state.filteredTasks.length === 0) {
+        return(<h4 style={{background:'#e0ffff'}}>No matches!</h4>);
+      }
+      return(
+        <div style={{background:'#e0ffff'}}>
+          <h4>Here is your filtered to do list:</h4>
+          <font size="2">
+            <table>
+              <thead align="center" >
+                <tr>
+                  <th>Id</th>
+                  <th>Text</th>
+                  <th>Status</th>
+                  <th>Tags</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.filteredTasks.map(task => 
+                  <tr>
+                    <td>
+                      <label>{task.id}</label>
+                    </td>
+                    <td>
+                      <label>{task.text}</label>
+                    </td>
+                    <td>
+                      <input type='checkbox' checked={task.isDone}></input>
+                    </td>
+                    <td>
+                      <label>{task.tags.join()}</label>
+                    </td>
+                  </tr>
+                )}
+              </tbody>
+            </table>
+          </font>
+        </div>
+      );
+    }
+
+    getFilteredTasks = () => {
+      let toDoList=this.state.toDoList;
+      let tagsForSearch=this.state.tagsForSearch;
+      let filteredTasks;
+      if(this.state.searchCondition==="AND"){
+        filteredTasks=this.getFilteredTasksWithAnd(toDoList, tagsForSearch);
+      }
+      else{
+        filteredTasks=this.getFilteredTasksWithOr(toDoList, tagsForSearch);
+      }
+      this.setState({filteredTasks: filteredTasks});
+    }
+
+    getFilteredTasksWithAnd = (toDoList, tagsForSearch) => {
+      return toDoList.filter(function(task) {return tagsForSearch.every(tag=>task.tags.includes(tag))});
+    }
+
+    getFilteredTasksWithOr = (toDoList, tagsForSearch) => {
+      return toDoList.filter(function(task) {return tagsForSearch.some(tag=>task.tags.includes(tag))});
+    }
   
     renderAddTaskTable = () => {
       return(
-        <table>
-          <tr>
-            <td>
-              <input type="text" onChange={this.changeTaskText} value={this.state.newTaskText} />
-            </td>
-            <td>
-              <input type="checkbox" onChange={this.changeTaskStatus} checked={this.state.newTaskStatus}/>
-            </td>
-            <td>
-              <input type="text" onChange={this.changeTaskTags} value={this.state.newTaskTags} />
-            </td>
-            <td>
-              <input type="button" defaultValue="Add task" onClick={this.addTask}/>
-            </td>
-          </tr>
-        </table>
+        <div style={{background:'#e6e6fa'}}>
+          <h4>Enter a new task: </h4>
+          <table>
+            <tr>
+              <td>
+                <input type="text" onChange={this.changeTaskText} value={this.state.newTaskText} />
+              </td>
+              <td>
+                <input type="checkbox" onChange={this.changeTaskStatus} checked={this.state.newTaskStatus}/>
+              </td>
+              <td>
+                <input type="text" onChange={this.changeTaskTags} value={this.state.newTaskTags} />
+              </td>
+              <td>
+                <input type="button" defaultValue="Add task" onClick={this.addTask}/>
+              </td>
+            </tr>
+          </table>
+        </div>
       )
     }
   
@@ -73,11 +175,11 @@ class ToDoList extends React.Component{
   
     renderToDoListTable = () => {
       if (this.state.toDoList.length === 0) {
-        return(<h5>Your to do list is empty</h5>);
+        return(<h4 style={{background:'#ffebcd'}}>Your to do list is empty</h4>);
       }
       return(
-        <div>
-          <h5>Here is your to do list:</h5>
+        <div style={{background:'#ffebcd'}}>
+          <h4>Here is your to do list:</h4>
           <font size="2">
             <table>
               <thead align="center" >
